32e91521a70e0dca01905a988ebd5f5de3566837
luis <lrivera2022@amsbronx.org>
2023-06-13 14:31:13 -0400

    fixed susu migration

--- src/db/migrations/20230612191245_susu.js
+++ src/db/migrations/20230612191245_susu.js
@@ -4,8 +4,11 @@
  */
 exports.up = (knex) => knex.schema.createTable('susu', (table) => {
     table.increments();
-    table.string('username').notNullable().unique();
+    table.string('name').notNullable();
     table.string('password_hash').notNullable();
+    table.string('owner');
+    table.string('payment-amount');
+    table.string('next-payment');
     table.timestamps(true, true);
   });
   
--- src/db/seeds/susu.js
+++ src/db/seeds/susu.js
@@ -4,10 +4,10 @@
  */
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
+  await knex('susu').del()
+  await knex('susu').insert([
+    {id: 1, name: 'rowValue1', password_hash :'123'},
+    {id: 2, name: 'rowValue2', password_hash :'123'},
+    {id: 3, name: 'rowValue3', password_hash :'123'}
   ]);
 };
