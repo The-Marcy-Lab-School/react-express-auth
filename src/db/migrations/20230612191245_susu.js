f841b47459d55341c3ee65ed7bcd9af0953937b4
luis <lrivera2022@amsbronx.org>
2023-06-13 13:55:23 -0400

    fixed migrations

--- src/db/migrations/20230612191245_susu.js
+++ src/db/migrations/20230612191245_susu.js
@@ -2,14 +2,15 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = (knex) => {
-
-};
-
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = (knex) => {
-
-};
+exports.up = (knex) => knex.schema.createTable('susu', (table) => {
+    table.increments();
+    table.string('username').notNullable().unique();
+    table.string('password_hash').notNullable();
+    table.timestamps(true, true);
+  });
+  
+  /**
+   * @param { import("knex").Knex } knex
+   * @returns { Promise<void> }
+   */
+  exports.down = (knex) => knex.schema.dropTable('susu');
