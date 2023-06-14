b3a22783b452c1b57930ba32b24ae9c92196d809
luisc8487 <lf9904@gmail.com>
2023-06-14 13:33:46 -0400

    put back the default users

---
+++ src/db/seeds/01_init.js
@@ -0,0 +1,20 @@
+const User = require('../models/user');
+/**
+ *   table.string('username').notNullable().unique();
+  table.string('password_hash').notNullable();
+ *   table.string('email').notNullable();
+  table.string('pic');
+  table.string('name');
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.seed = async (knex) => {
+  // await User.deleteAll();
+  // await knex('grocery_list').del()
+  // await knex('likes').del()
+  // await knex('comments').del()
+  // await User.create('cool_cat', '124');
+  await User.create('cool_cat', '124');
+  await User.create('l33t-guy', '1234');
+  await User.create('wowow', '1235');
+};
