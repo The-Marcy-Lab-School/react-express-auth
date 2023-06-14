dec1117c0f296fd30de3852e6ed43933eca855eb
“Xhesika” <xhesikam2184@tywls.org>
2023-06-14 15:14:13 -0400

    port

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const server = require('./server');
 
-const port = process.env.PORT || 5173;
+const port = process.env.PORT || 3000;
 const host = process.env.HOST || '127.0.0.1';
 
 server.listen(port, host, () => {
