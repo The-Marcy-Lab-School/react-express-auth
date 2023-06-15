56f1b8cd3c49ced4890d9da1744879acf29cb62d
Duojay Williams <dhwilliams3007@gmail.com>
2023-06-15 11:07:43 -0400

    update

--- src/controllers/events/delete.js
+++ src/controllers/events/delete.js
@@ -1,8 +1,8 @@
-const delete = async (id) => {
-    // call the delete method of the PostedImages class and pass in the id
-    const result = await Events.delete(id);
-    // return a boolean indicating if the deletion was successful or not
-    return result ? true : false;
-  };
+// const delete = async (id) => {
+//     // call the delete method of the PostedImages class and pass in the id
+//     const result = await Events.delete(id);
+//     // return a boolean indicating if the deletion was successful or not
+//     return result ? true : false;
+//   };
   
-    module.exports = delete;
+//     module.exports = delete;
--- src/controllers/events/index.js
+++ src/controllers/events/index.js
@@ -1,8 +1,8 @@
 const create = require('./create');
 const list = require('./list');
-const delete = require('./delete');
+// const delete = require('./delete');
 module.exports = {
   create,
   list,
-  delete
+  // delete
 };
