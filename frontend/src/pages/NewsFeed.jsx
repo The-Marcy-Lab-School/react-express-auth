eff1729b01a78df1ee5672b7f7432ba340c0fb9a
Magdalena May Gero <114107908+magdalenamaygero@users.noreply.github.com>
2023-06-14 23:56:48 -0400

    newsfeed fetches

--- frontend/src/pages/NewsFeed.jsx
+++ frontend/src/pages/NewsFeed.jsx
@@ -5,9 +5,15 @@ const Article = ({ article }) => {
     window.open(article.web_url, '_blank');
   };
   return (
+    <div class="card">
+  <div class="card-content">
+    <div class="content">
     <div onClick={handleClick}>
       <h2>{article.headline.main}</h2>
       <p>{article.abstract}</p>
+    </div>
+    </div>
+    </div>
     </div>
   );
 };
@@ -35,7 +41,7 @@ const ArticleList = () => {
   }, []);
 
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', height: '100vh' }}>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', height: '100vh', overflow: 'auto'}}>
       <h1>NewsFeed</h1>
       <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
         {articles.map(article => (
