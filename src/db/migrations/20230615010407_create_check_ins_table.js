c9b996382ad1c4c072fd65130a2caf304a031373
Mohammed <mohammeduddin1998@gmail.com>
2023-06-15 12:15:42 -0400

    .

--- src/db/migrations/20230615010407_create_check_ins_table.js
+++ src/db/migrations/20230615010407_create_check_ins_table.js
@@ -2,7 +2,7 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = (knex) => knex.schema.createTable('checkIns', (table) => {
+exports.up = (knex) => knex.schema.createTable('check-ins', (table) => {
     table.increments();
     table.integer('event_id').references('id').inTable('events');  
     table.integer('user_id').references('id').inTable('users');
