6d375424c5b9b4c82fa85c246920b1a0e0f8fb55
treyjaded <trevonjcharles@gmail.com>
2023-06-15 19:55:38 -0400

    Added Map & Events component

---
+++ frontend/src/components/Events.jsx
@@ -0,0 +1,104 @@
+/* eslint-disable max-len */
+/* eslint-disable no-unused-vars */
+/* eslint-disable func-style */
+import React, { useEffect, useState, useContext } from 'react';
+// import { DataContext } from "test-react/src/assets/DataContext.jsx";
+
+// ------------------List of Events ----------------
+function EventList() {
+  const [events, setEvents] = useState([]);
+  // const { data, setData } = useContext(DataContext); // Use context to store data through components
+  // setData(events);
+  // console.log("EVENTDATA:",events)
+
+  useEffect(() => {
+    fetchEvents();
+  }, []);
+
+  // console.log("Longitude:",eventData[0].geometry[0].coordinates[1])
+
+  const fetchEvents = () => {
+    fetch('https://eonet.gsfc.nasa.gov/api/v3/events?status=open&limit=10')
+      .then((response) => response.json())
+      .then((data) => {
+        setEvents(data.events);
+        // console.log("DATA:",data)
+        // const latitude = events[0].geometry[0].coordinates[0];
+        // console.log(latitude);
+      })
+    // console.log("DATA:",data)                                                    //GENERAL DATA
+    // console.log("TYPE:",data.events[0].categories[0].title)                  //Type of Hazard Events
+
+      .catch((error) => console.log(error));
+  };
+
+  //  const latitude = events[0].geometry[0].coordinates[0]
+  //  console.log(latitude)
+  //  const Longitude = eventData[0].geometry[0].coordinates[1]
+
+  return (
+    <dl className='eventList'>
+      {events.map((event) => (
+        <React.Fragment key={event.id}>
+          <li ><a href="#">{event.categories.map((category) => <div key = {category.id}>{category.title}</div>)} {event.title}</a></li>
+          {event.description && (
+            <dd><em>{event.description}</em></dd>
+          )}
+        </React.Fragment>
+      ))}
+    </dl>
+  );
+}
+
+// ------------------List of Layers ----------------
+function LayerList() {
+  const [layers, setLayers] = useState([]);
+
+  useEffect(() => {
+    fetchLayers();
+  }, []);
+
+  const fetchLayers = () => {
+    const eventId = "EONET_182";
+    fetch(`https://eonet.gsfc.nasa.gov/api/v3/events/${eventId}?status=open&limit=5`)
+      .then((response) => response.json())
+      .then((event) => {
+        const { categories } = event;
+        const fetchLayersPromises = categories.map((category) => {
+          const layerMapAPI = `https://eonet.gsfc.nasa.gov/api/v3/layers/${category.id}?status=open&limit=5`;
+          // console.log("event:", event)
+          return fetch(layerMapAPI).then((response) => response.json());
+        });
+
+        Promise.all(fetchLayersPromises)
+          .then((layersData) => {
+            const layers = layersData.flatMap((data) => data.categories[0].layers);
+            setLayers(layers);
+          })
+          .catch((error) => console.log(error));
+      })
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <React.Fragment>
+      <div id="eventTitle"></div>
+      <dl id="layerList">
+        {layers.map((layer) => (
+          <React.Fragment key={layer.id}>
+            <dt>{layer.name}</dt>
+          </React.Fragment>
+        ))}
+      </dl>
+    </React.Fragment>
+  );
+}
+
+function Event() {
+  return (
+//div className='eventList'>
+     <EventList />
+  );
+}
+
+export default Event;
---
+++ frontend/src/components/MapComponent.jsx
@@ -0,0 +1,76 @@
+import React, { useEffect, useContext, useState} from 'react';
+import mapboxgl from 'mapbox-gl';
+import Event from './Events';
+import 'mapbox-gl/dist/mapbox-gl.css';
+// import { DataContext } from '/Users/jaded/Development/test react/src/assets/DataContext.jsx'
+
+const MapComponent = () => {
+  // const [latitude, setLatitude] = useState([]);
+  // const { data } = useContext(DataContext)
+
+  // console.log(data)
+  // const latitude = data[0]
+
+  // const { setData } = useContext(DataContext); //Use context to store data through components
+  // const eventData = latitude;
+
+  // setData(eventData);
+  // console.log("sajckanscjn:")
+
+  useEffect(() => {
+    mapboxgl.accessToken = 'pk.eyJ1IjoidHJleWphZGVkIiwiYSI6ImNsaXRkYWV4czAxa28za3QzeWgzcnB5YnAifQ.Sa0yc1I-LsaVBVMIPLYzxA';
+    const map = new mapboxgl.Map({
+      container: 'map', // container ID
+      style: 'mapbox://styles/treyjaded/cliwe9c1002ak01qhag512fac', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+    //  const popup = new mapboxgl.Popup({ closeOnClick: false })
+    map.on('load', () => {
+      new mapboxgl.Popup({ closeOnClick: false })
+        .setLngLat([-74.5, 40])
+        .setHTML('<h1>Hello World!</h1>')
+        .addTo(map);
+
+      new mapboxgl.Popup({ closeOnClick: false })
+        .setLngLat([-123.9749, 40.7736])
+        .setHTML('<h1>Hey World!</h1>')
+        .addTo(map);
+
+      new mapboxgl.Popup({ closeOnClick: false })
+        .setLngLat([14, 23])
+        .setHTML('<h1>Bonjour le monde!</h1>')
+        .addTo(map);
+
+      new mapboxgl.Popup({ closeOnClick: false })
+        .setLngLat([-97.91772901156455, 18.988768526321365])
+        .setHTML('<h1>Hola Mundo!</h1>')
+        .addTo(map);
+
+      new mapboxgl.Popup({ closeOnClick: false })
+        .setLngLat([-67.67617340843067, -33.80002186085345])
+        .setHTML('<h1>Olá Mundo!</h1>')
+        .addTo(map);
+
+      new mapboxgl.Popup({ closeOnClick: false })
+        .setLngLat([19.35052445717912, 64.63980479322365])
+        .setHTML('<h1>Witaj świecie!</h1>')
+        .addTo(map);
+
+      new mapboxgl.Popup({ closeOnClick: false })
+        .setLngLat([115.9518135866121, 63.20184619910259])
+        .setHTML('<h1>Привет, мир!</h1>')
+        .addTo(map);
+    });
+
+    return () => {
+      map.remove();
+    };
+  }, []);
+
+  return (
+      <div id="map" ></div>
+  );
+};
+
+export default MapComponent;
