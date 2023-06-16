4d504bd7f34e9078d9dac5382200f2e356ac0a6c
luisc8487 <lf9904@gmail.com>
2023-06-16 16:49:59 -0400

    fixed Issac html semantics from home login signup and className

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,16 @@
 export default function HomePage() {
-  return <body class='sign-bg'>
-    <div id='moto-div'>
-      <h1 class='moto-text'>Transforming lives,</h1>
-      <h1>1 meal at a time</h1>
-    </div>
-    <div id='story-div'>
-      <p>Just eat healthy or ill crawl from under your bed at night...</p>
-    </div>
-  </body>;
+  return (
+    <>
+      <div className="sign-bg">
+        <div id="moto-div">
+          <h1 className="moto-text">Transforming lives,</h1>
+          <h1>1 meal at a time</h1>
+        </div>
+        <div id="story-div">
+          <p>Just eat healthy or ill crawl from under your bed at night...</p>
+        </div>
+      </div>
+      ;
+    </>
+  );
 }
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -5,14 +5,16 @@ import CurrentUserContext from "../contexts/current-user-context";
 
 export default function LoginPage() {
   const navigate = useNavigate();
-  const [errorText, setErrorText] = useState('');
+  const [errorText, setErrorText] = useState("");
   const { currentUser, setCurrentUser } = useContext(CurrentUserContext);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    setErrorText('');
+    setErrorText("");
     const formData = new FormData(event.target);
-    const [user, error] = await logUserIn(Object.fromEntries(formData.entries()));
+    const [user, error] = await logUserIn(
+      Object.fromEntries(formData.entries())
+    );
     if (error) return setErrorText(error.statusText);
     setCurrentUser(user);
     navigate(`/users/${user.id}`);
@@ -20,17 +22,31 @@ export default function LoginPage() {
 
   if (currentUser) return <Navigate to="/" />;
 
-  return <body class='sign-bg'>
-    <form class='user-form' onSubmit={handleSubmit}>
-      <h1>Login</h1>
-      <label htmlFor="username">Username</label>
-      <input type="text" autoComplete="username" id="username" name="username" />
+  return (
+    <>
+      <div className="sign-bg">
+        <form className="user-form" onSubmit={handleSubmit}>
+          <h1>Login</h1>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            autoComplete="username"
+            id="username"
+            name="username"
+          />
 
-      <label htmlFor="password">Password</label>
-      <input type="password" autoComplete="current-password" id="password" name="password" />
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            autoComplete="current-password"
+            id="password"
+            name="password"
+          />
 
-      <button>Log in!</button>
-    </form>
-    { !!errorText && <p>{errorText}</p> }
-  </body>;
+          <button>Log in!</button>
+        </form>
+        {!!errorText && <p>{errorText}</p>}
+      </div>
+    </>
+  )
 }
