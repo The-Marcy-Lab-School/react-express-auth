61101dd6183f4c0e7f041f33fabf3bc450f9fcbc
Magdalena May Gero <114107908+magdalenamaygero@users.noreply.github.com>
2023-06-16 18:28:44 -0400

    second fetch

--- frontend/src/pages/NewsFeed.jsx
+++ frontend/src/pages/NewsFeed.jsx
@@ -5,33 +5,52 @@ const Article = ({ article }) => {
     window.open(article.web_url, '_blank');
   };
   return (
-    <div class="card">
-  <div class="card-content">
-    <div class="content">
-    <div onClick={handleClick}>
-      <h2>{article.headline.main}</h2>
-      <p>{article.abstract}</p>
-    </div>
-    </div>
-    </div>
+    <div className="card">
+      <div className="card-content">
+        <div className="content">
+          <div onClick={handleClick}>
+            <h2>{article.headline.main}</h2>
+            <p>{article.abstract}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
 const ArticleList = () => {
   const [articles, setArticles] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('https://api.nytimes.com/svc/search/v2/articlesearch.json?q=environment&api-key=jpOGZTJ7dvp2jenrZozAWBi37rXc0sJo');
+        const response1 = fetch(
+          'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=environment&api-key=jpOGZTJ7dvp2jenrZozAWBi37rXc0sJo'
+        );
+        const response2 = fetch(
+          'https://newsdata.io/api/1/news?apikey=pub_24581aea2419d64bc1af13fab0cfa12222033&q=environment&language=en&category=environment'
+        );
 
-        if (!response.ok) {
+        const [data1, data2] = await Promise.all([response1, response2]);
+
+        if (!data1.ok || !data2.ok) {
           throw new Error('Error fetching articles');
         }
 
-        const data = await response.json();
-        setArticles(data.response.docs);
+        const articles1 = await data1.json();
+        const articles2 = await data2.json();
+
+        const articlesFromSecondAPI = articles2.data.results;
+
+        // Combine articles from both APIs
+        const combinedArticles = [
+          ...articles1.response.docs,
+          ...articlesFromSecondAPI,
+        ];
+
+        setArticles(combinedArticles);
+        setIsLoading(false);
       } catch (error) {
         console.log(error);
       }
@@ -41,16 +60,28 @@ const ArticleList = () => {
   }, []);
 
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', overflow: 'auto', marginTop: '30px', marginBottom: '20px'}}>
-      <h1 style={{ margin: '20px' }} >NewsFeed</h1>
-      <div style={{ display: 'flex', flexDirection: 'column'}}>
-        {articles.map(article => (
-          <Article key={article._id} article={article}/>
-        ))}
-      </div>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+        overflow: 'auto',
+        marginTop: '30px',
+      }}>
+      <h1 style={{ margin: '20px' }}>NewsFeed</h1>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          {articles.map((article) => (
+            <Article key={article._id} article={article} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
 
-export default ArticleList;
 
+export default ArticleList;
