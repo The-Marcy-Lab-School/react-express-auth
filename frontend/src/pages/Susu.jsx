d22182f12a463008973ec71adb77269da79553fc
Afairman2003 <afairman@brooklynsteamcenter.org>
2023-06-17 21:01:20 -0400

    different changes

--- frontend/src/pages/Susu.jsx
+++ frontend/src/pages/Susu.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect, useContext } from 'react';
 import CurrentSusuContext from '../contexts/susu-context';
 import SusuCard from '../components/SusuCard';
+import '../styles/susus-page.css';
+
 
 export default function Susu() {
   const { currentSusulist, setCurrentSusulist } = useContext(CurrentSusuContext);
@@ -11,7 +13,7 @@ export default function Susu() {
     <>
       {currentSusulist.map((susu) => (
         // console.log(susu)
-        <SusuCard key={susu.id} susu={susu} />
+        <SusuCard className="susu-card" key={susu.id} susu={susu} />
       ))}
     </>
   );
--- src/db/seeds/susu.js
+++ src/db/seeds/susu.js
@@ -6,10 +6,10 @@ exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('susu').del()
   await knex('susu').insert([
-    {id: 1, name: 'Mueller Group', password_hash :'mueller', owner: 2},
-    {id: 2, name: 'Hoeger Group', password_hash :'hoeger', owner: 4},
-    {id: 3, name: 'Hickle and Sons', password_hash :'hickle', owner: 1},
-    {id: 4, name: 'Schuster Inc', password_hash :'schuster', owner: 3}
+    {id: 1, name: 'Mueller Group', password_hash :'mueller', owner: 2, payment_amount: 1234, next_payment: 1231},
+    {id: 2, name: 'Hoeger Group', password_hash :'hoeger', owner: 4,payment_amount: 1234, next_payment: 1231},
+    {id: 3, name: 'Hickle and Sons', password_hash :'hickle', owner: 1,payment_amount: 1234, next_payment: 1231},
+    {id: 4, name: 'Schuster Inc', password_hash :'schuster', owner: 3,payment_amount: 1234, next_payment: 1231}
     
   ]);
 };
