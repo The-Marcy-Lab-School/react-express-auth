acaae617379ed134fcc9a03e1ab38d873e6fcad7
OrwRandal <randypichardo1987@gmail.com>
2023-06-19 13:15:23 -0400

    designed sign up page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -37,7 +37,7 @@ export default function LoginPage() {
     <div id="logInContainer">
       <div id='logInBox'>
         <form onSubmit={handleSubmit} id="logInForm">
-          <h1 className="title">Log In</h1>
+          <h1 className="title has-text-centered">Log In</h1>
           <div id='errorTextDiv'>
             <label className="is-size-5" htmlFor='username'>Username</label>
             {!!errorText && <p className="has-text-danger">{errorText}</p>}
@@ -45,10 +45,8 @@ export default function LoginPage() {
           <input type="text" className='input is-medium' placeholder="Username" id='username' name='username'></input>
           <label className="is-size-5" htmlFor='password'>Password</label>
           <input type="password" className='input is-medium' id='password' name='password' placeholder="Password"></input>
-          <button type='submit' className="button logInButton mb-3 is-rounded">Log In</button>
-          <Link to='/sign-up'>
-            <p className="has-text-centered has-text-black" id='clickToSignUp'>Don't have an account? Click here to sign up</p>
-          </Link>
+          <button type='submit' className="button logInButton mb-2 is-rounded is-custom">Log In</button>
+          <p className="has-text-centered">Don't have an account? Click here to <Link to="/sign-up">Sign Up!</Link></p>
         </form>
       </div>
     </div>
