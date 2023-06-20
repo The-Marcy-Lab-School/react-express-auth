a90a8d5234abe0c1fe6e2a60ca499d8630eb10f1
colivie000 <casterlyo@gmail.com>
2023-06-20 10:56:35 -0400

    updated seeds

--- src/db/seeds/susu.js
+++ src/db/seeds/susu.js
@@ -8,8 +8,11 @@ exports.seed = async function(knex) {
   await knex('susu').insert([
     {id: 1, name: 'Mueller Group', password_hash :'mueller', owner: 2},
     {id: 2, name: 'Hoeger Group', password_hash :'hoeger', owner: 4},
-    {id: 3, name: 'Hickle and Sons', password_hash :'hickle', owner: 1},
-    {id: 4, name: 'Schuster Inc', password_hash :'schuster', owner: 3}
+    {id: 3, name: 'Hickle and Sons', password_hash :'hickle', owner: 5},
+    {id: 4, name: 'Schuster Inc', password_hash :'schuster', owner: 3},
+    {id: 5, name: 'Silicon Valley', password_hash :'silicon', owner: 2},
+    {id: 6, name: 'Franklin Square', password_hash :'franklin', owner: 1},
+    {id: 7, name: 'Marcy Group', password_hash :'marcy', owner: 1}
     
   ]);
 };
