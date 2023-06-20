44a5c71fc9403e1428e1e32b2f2da58e9d1f8f4c
saidabary <saidabary05@gmail.com>
2023-06-20 13:38:09 -0400

    added facilities in seeds _pages

--- src/db/seeds/pages.js
+++ src/db/seeds/pages.js
@@ -146,5 +146,133 @@ exports.seed = async function (knex) {
       photo:
         "https://www.aldamedicalcenter.com/wp-content/uploads/2015/06/AMC-Lawrence-200x200.jpg",
     },
+    {
+      user_id: 27,
+      facility_doctor: "CityMD Sunset Park Urgent Care ",
+      specialty: "Urgent Care",
+      description:
+        "CityMD is the largest provider of urgent cares in New York and New Jersey",
+      address: "5024 5th Ave, Brooklyn, NY ",
+      overall_rating: 2,
+      is_facility: true,
+      is_doctor: false,
+      photo:
+        "https://lh3.googleusercontent.com/p/AF1QipNmHwHbmiC2UF9MvhGf-ZlRfWA_nSNiUWU5oR4Q=s1360-w1360-h1020",
+    },
+
+    {
+   
+      user_id: 18,
+      facility_doctor: "NYU Langone Hospital Brooklyn",
+      specialty: "Hospital",
+      description:
+        "A full-service, 450-bed academic teaching hospital in the Sunset Park neighborhood of Brooklyn",
+      address: "150 55th St, Brooklyn, NY",
+      overall_rating: 4,
+      is_facility: true,
+      is_doctor: false,
+      photo:
+        "https://lh3.googleusercontent.com/p/AF1QipMLyzb9SauEF_khxV-1F7CQvoOHLsv4j40TY3rB=s1360-w1360-h1020",
+    },
+
+    {
+      user_id: 15,
+      facility_doctor: "NYC Health + Hospitals/South Brooklyn Health",
+      specialty: "Hospital",
+      description: "Public teaching hospital located in the Coney Island neighborhood of Brooklyn",
+      address: "2601 Ocean Pkwy, Brooklyn, NY 11235",
+      overall_rating: 4,
+      is_facility: true,
+      is_doctor: false,
+      photo:
+        "https://lh3.googleusercontent.com/p/AF1QipPYotk4CbOXlPxENAAipHF7Hmfmfo56APE239Xu=s1360-w1360-h1020",
+    },
+    {
+      user_id: 10,
+      facility_doctor: "Dr Ye's Medical Office",
+      specialty: "Doctor's Office ",
+      description: "Family Medicine",
+      address: "822 54th St, Brooklyn, NY 11220",
+      overall_rating: 2,
+      is_facility: true,
+      is_doctor: false,
+      photo:
+        "https://streetviewpixels-pa.googleapis.com/v1/thumbnail?panoid=CpKlm1y_gwYAnu6QSQcwAA&cb_client=search.gws-prod.gps&yaw=224.82936&pitch=0&thumbfov=100&w=650&h=218",
+    },
+    {
+      user_id: 16,
+      facility_doctor: "Huang Medical Care PC",
+      specialty: "Doctor's Office ",
+      description: "Family Care Medical Clinic",
+      address: "4620 3rd Ave, Brooklyn, NY 11220",
+      overall_rating: 3,
+      is_facility: true,
+      is_doctor: false,
+      photo:
+        "https://streetviewpixels-pa.googleapis.com/v1/thumbnail?panoid=AEr68817L6NVHwhLcLrQVw&cb_client=search.gws-prod.gps&yaw=347.6144&pitch=0&thumbfov=100&w=650&h=218",
+    },
+{
+    user_id: 5,
+    facility_doctor: "Maimonides Medical Center ",
+    specialty: "Hospital ",
+    description: " non-profit, non-sectarian hospital located in Borough Park, in the New York City borough of Brooklyn, in the U.S. state of New York",
+    address: "4802 10th Ave, Brooklyn, NY 11219",
+    overall_rating: 5,
+    is_facility: true,
+    is_doctor: false,
+    photo:
+      "https://lh3.googleusercontent.com/p/AF1QipNHkorJIXPzMxwLribNIcuswpweFr8f1CwnWdRv=s1360-w1360-h1020",
+  },
+  {
+    user_id: 3,
+    facility_doctor: "Center for Community Health at NewYork-Presbyterian Brooklyn Methodist Hospital",
+    specialty: "Hospital ",
+    description: " Largest ambulatory care facility in Brooklyn ",
+    address: "515 6th St, Brooklyn, NY 11215",
+    overall_rating: 1,
+    is_facility: true,
+    is_doctor: false,
+    photo:
+      "https://lh3.googleusercontent.com/p/AF1QipPri-Y0p6sNPOVbrojeE7BhWojDTUtV9jXQuSqh=s1360-w1360-h1020",
+  },
+  {
+    user_id: 13,
+    facility_doctor: "The Brooklyn Hospital Center",
+    specialty: "Hospital ",
+    description: "Full-service community teaching hospital",
+    address: "121 Dekalb Ave, Brooklyn, NY 11201",
+    overall_rating: 4,
+    is_facility: true,
+    is_doctor: false,
+    photo:
+      "https://lh3.googleusercontent.com/p/AF1QipO4Q6CG-qeWJH1HoJZOOPlrOM9OvXsVPvzvbYi3=s1360-w1360-h1020",
+  },
+  {
+    user_id: 9,
+    facility_doctor: "Kings County Hospital Center ",
+    specialty: "Hospital",
+    description: "Owned and operated by NYC Health + Hospitals, a municipal agency that runs New York City's public hospitals. ",
+    address: "451 Clarkson Ave, Brooklyn, NY 11203",
+    overall_rating: 4,
+    is_facility: true,
+    is_doctor: false,
+    photo:
+      "https://lh3.googleusercontent.com/p/AF1QipOI1nBdV4nVWfNJaJMpFTc1k4G8i4bB1hEgPVZU=s1360-w1360-h1020",
+  },
+  
+  {
+    user_id: 12,
+    facility_doctor: "Ambulatory Care",
+    specialty: "Hospital",
+    description: " Family Care Hospital in NewYork-Presbyterian Brooklyn Methodist Hospital",
+    address: " 506 6th St, Brooklyn, NY 11215",
+    overall_rating: 4,
+    is_facility: true,
+    is_doctor: false,
+    photo:
+      "https://lh3.googleusercontent.com/p/AF1QipOI1nBdV4nVWfNJaJMpFTc1k4G8i4bB1hEgPVZU=s1360-w1360-h1020",
+  }
+
   ]);
+  
 };
