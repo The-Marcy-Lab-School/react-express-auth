7aadbae5e690899c79e8712595f8059b379cf696
arlyy15 <raymundoarly15@gmail.com>
2023-06-20 14:21:07 -0400

    added seeds

--- src/db/migrations/20230613192858_create_grocery_list.js
+++ src/db/migrations/20230613192858_create_grocery_list.js
@@ -4,7 +4,7 @@
  */
 exports.up = (knex) => {
     return knex.schema.createTable("grocery_list", (table) => {
-        table.increments()
+        table.increments();
         table.string("nova_rate");
         table.integer("nutri_score");
         table.timestamp("created_at").defaultTo(knex.fn.now());
