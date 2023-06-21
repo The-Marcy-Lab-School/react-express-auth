2963daf5d4599823ce439af397070a37dcf11853
arlyy15 <raymundoarly15@gmail.com>
2023-06-21 15:54:51 -0400

    update

--- src/controllers/grocerylist/create.js
+++ src/controllers/grocerylist/create.js
@@ -1,11 +1,11 @@
 const createGrocery = async (req, res) => {
   const {
     db: { Grocery_list },
-    body: { nova_rate, nutri_score },
+    body: { list_name,nova_rate, nutri_score },
   } = req;
 
-  const Grocery = await Grocery_list.create( nova_rate, nutri_score);
-  // session.userId = user.id;
+  const Grocery = await Grocery_list.create( list_name,nova_rate, nutri_score);
+  
 
   res.send( Grocery );
 };
