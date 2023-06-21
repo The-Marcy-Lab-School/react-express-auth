8f9ad4d5e856030772e2ff79c50a0e81805a2000
Swager0932 <swagerbot2000@gmail.com>
2023-06-21 19:43:52 -0400

    404 Page Completed

--- frontend/src/pages/Page404.jsx
+++ frontend/src/pages/Page404.jsx
@@ -1,7 +1,26 @@
+
+import foodParty from "../media/mrbroc.gif";
+import error from "../media/404.png";
+
+
 export default function Page404(){
     return (
-        <>
-            <h1>Page Not Found</h1>
-        </>
+        <body id="Page404">
+            <style>
+                @import url('https://fonts.googleapis.com/css2?family=Nunito&display=swap');
+            </style>
+
+            <div id="lostlol"> 
+            
+                <img id="errordiv" src={error}></img>
+
+                <h1 className="h1404">Seems like you're lost,</h1> 
+                <h1 className="h1404">Mr. Broccoli is here to guide you back!</h1>
+                <img id="mrbroc" src={foodParty}></img>
+                <a id="returnButt" href="http://localhost:5173"> Follow Mr. Broccoli </a>
+            </div>
+            
+
+        </body>
     )
 }
