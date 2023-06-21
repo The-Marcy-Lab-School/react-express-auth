c497f1413c8148f706a4d0df9454db38925d12c1
Magdalena May Gero <114107908+magdalenamaygero@users.noreply.github.com>
2023-06-21 12:15:10 -0400

    landing page text

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,13 +2,13 @@ import { Link } from 'react-router-dom'
 export default function HomePage() {
   return <>
     <section className='section is-large' id='landingSection1'>
-      <h1 className='has-text-weight-bold is-size-1 has-text-centered'>Empowering Communities for Sustainable Recycling: Inspiring Action, Fostering Change</h1>
+      <h1 className='has-text-weight-bold is-size-1 has-text-centered'>Empowering Communities for Sustainable Recycling: Inspiring Action and Fostering Change</h1>
     </section>
     <section className='section is-medium has-text-centered' id='landingSection2'>
-      <h1 className='is-size-3 has-text-weight-bold'>Together, we create a passionate community focused on recycling, preserving resources, and building a sustainable planet through engaging events.</h1>
-      <p className='is-size-4 py-5 pb-5'>Join us in driving positive change!</p>
+      <h1 className='is-size-3 has-text-weight-bold'>Discover and join fun community events in any NYC borough! This feature brings you the opportunity to join cleanups, exchanges and more, right at your fingertips.</h1>
+      <p className='is-size-4 py-5 pb-5'>Browse events, and easily join to make a positive impact in your own neighborhood!</p>
       <Link to='/events'>
-        <button id='landingCreateButton' className='button'>Create an Event</button>
+        <button id='landingCreateButton' className='button'>Create Event</button>
       </Link>
     </section>
     <section className='section is-medium' id='landingSection3'>
@@ -18,7 +18,7 @@ export default function HomePage() {
       <p>Our mission is to create an online platform that empowers individuals and communities to actively participate in sustainable recycling practices. Through our recycling event webpage, we aim to inspire action, raise awareness, and foster a sense of responsibility towards the environment.</p>
       <br/>
       <Link to='/about' className='aboutLink'>
-      <p className='is-size-5 py-4 pb-4 is-underlined'>Learn more about us</p>
+      <p className='is-size-5 py-4 pb-4 is-underlined'>Learn More Abbout Us</p>
       </Link>
       </div>
       <figure className="image">
@@ -29,7 +29,7 @@ export default function HomePage() {
       <h1 className='is-size-3 has-text-weight-bold'>Read The Latest News on Enironmentalism</h1>
       <p className='is-size-4 py-5 pb-5'>Our NewsFeed brings you the latest updates, informative articles, and statistics on environmental issues. Discover practical tips for sustainable living and join a community of like-minded individuals. Dive into comprehensive statistics highlighting the current state of our environment. Become part of our global movement for a greener, cleaner future!</p>
       <Link to='/news'>
-        <button id='landingCreateButton' className='button'>Read the News</button>
+        <button id='landingCreateButton' className='button'>Browse News</button>
       </Link>
     </section>
   </>
