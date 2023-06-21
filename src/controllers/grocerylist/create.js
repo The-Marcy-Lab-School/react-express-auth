546314a19f37478d66ac383fc85f54507eafcb60
“Xhesika” <xhesikam2184@tywls.org>
2023-06-21 16:34:13 -0400

    correct

--- src/controllers/grocerylist/create.js
+++ src/controllers/grocerylist/create.js
@@ -1,10 +1,10 @@
 const createGrocery = async (req, res) => {
   const {
     db: { Grocery_list },
-    body: { list_name,nova_rate, nutri_score },
+    body: { list_name, nova_rate, nutri_score },
   } = req;
 
-  const Grocery = await Grocery_list.create( list_name,nova_rate, nutri_score);
+  const Grocery = await Grocery_list.create( list_name, nova_rate, nutri_score);
   
 
   res.send( Grocery );
--- src/routes.js
+++ src/routes.js
@@ -23,7 +23,6 @@ Router.get('/logged-in-secret', checkAuthentication, (req, res) => {
 });
 //grocery routes
 Router.post('/grocerylist', groceryController.create);
-
 Router.get('/grocerylist/:id', groceryController.list);
 Router.get('/grocerylist/:id/:userid', groceryController.listItems);
 Router.delete('/grocerylist/:id', groceryController.destroy);
