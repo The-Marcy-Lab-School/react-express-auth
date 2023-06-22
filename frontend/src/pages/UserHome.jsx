d45d88f0eb42549298c8a0ac7ac40b92eb8e9605
Swager0932 <swagerbot2000@gmail.com>
2023-06-21 21:49:41 -0400

    User Homepage semi done

--- frontend/src/pages/UserHome.jsx
+++ frontend/src/pages/UserHome.jsx
@@ -2,7 +2,8 @@ import { useContext } from "react";
 import ProductContext from "../contexts/ProductContext";
 import ProductsList from "./ProductsList"
 
-export default function UserHomePage() {
+
+export default function Page404(){
     const { setQuery } = useContext(ProductContext);
    
     const handleSubmit = async (e) => {
@@ -11,15 +12,21 @@ export default function UserHomePage() {
         setQuery(e.target[0].value);
     }
 
-    return <>
-      <h1>User Home Page</h1>
-      <p>Put something interesting here!</p>
+    return <body id="search-page">
+            <style>
+                @import url('https://fonts.googleapis.com/css2?family=Nunito&display=swap');
+            </style>
 
-      <form onSubmit={handleSubmit}>
-      <h1>Search Form</h1>
-        <input type="text"  placeholder="Enter your food"/>
-        <button type="submit">Search</button>
-      </form>
-      <ProductsList/>
-    </>;
-  }
+        <div id="search-food-area">
+            <form id="search-food-form" onSubmit={handleSubmit}>
+            <h1 id="search-text">Search</h1>
+            <div id="search-food-box">
+                <input type="text"  placeholder="Enter your food"/>
+                <button type="submit">Search</button>
+            </div>
+            </form>
+            <ProductsList/>
+        </div>
+
+    </body>;
+}
