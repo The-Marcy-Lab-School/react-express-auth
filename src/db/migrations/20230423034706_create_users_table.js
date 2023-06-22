a6cfa6ab7bc7a10f3962ac615e0c56fb0468d083
NicoDaSkater <nicolas.y.nunezbaez@gmail.com>
2023-06-21 22:50:04 -0400

    things

--- src/db/migrations/20230423034706_create_users_table.js
+++ src/db/migrations/20230423034706_create_users_table.js
@@ -6,7 +6,7 @@ exports.up = (knex) => knex.schema.createTable('users', (table) => {
   table.increments();
   table.string('username').notNullable().unique();
   table.string('password_hash').notNullable();
-  table.string('city');
+  table.string('');
   table.string('state');
   table.boolean('is_safe');
   table.timestamps(true, true);
