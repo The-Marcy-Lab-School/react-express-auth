de47e832e406fa6da022a1c52902bba5ee0a2366
NicoDaSkater <nicolas.y.nunezbaez@gmail.com>
2023-06-25 15:37:17 -0400

    time to implements disasters and near by users in fron-end

--- src/controllers/event/list.js
+++ src/controllers/event/list.js
@@ -1,15 +1,16 @@
 const { Events } = require("pg");
-const  classifyPoint = require("robust-point-in-polygon")
+const turf = require("turf")
+const inPolygon = require("@turf/boolean-point-in-polygon")
 const listEvents = async (req, res) => {
     const {
         session: {userId},
         db : { User },
     } = req;
 
+   
 const fetchData = async (url) => {
   try {
     const response = await fetch(url); // use fetch
-    console.log(response)
     const data = await response.json(); // convert incoming json data to js object
     return data; // return that data
   } catch (error) {
@@ -17,50 +18,42 @@ const fetchData = async (url) => {
     return null;
   }
 };
-const getIsPointInsidePolygon = (point, vertices ) => {
-  const x = point[0]
-  const y = point[1]
-  
-  let inside = false
-  for (let i = 0, j = vertices.length - 1; i < vertices.length; j = i++) {
-    const xi = vertices[i][0],
-      yi = vertices[i][1]
-    const xj = vertices[j][0],
-      yj = vertices[j][1]
-  
-    const intersect = yi > y != yj > y && x < ((xj - xi) * (y - yi)) / (yj - yi) + xi
-    if (intersect) inside = !inside
-  }
-  
-  return inside
-  }
     const processed = []
 
-    const { features : events } = await fetchData('https://api.weather.gov/alerts/active?severity=Severe&limit=1')
+    const { features : disasters } = await fetchData('https://api.weather.gov/alerts/active?severity=Severe&limit=1')
     const users = await fetchData('http://localhost:3000/api/users')
-    const filtered = events.filter( event => !!event.geometry)
-    filtered.forEach(event => {
-      const { geometry: { coordinates : [ coords ] } } = event 
-      // console.log(coords)
+    const filtered = disasters.filter( disaster => !!disaster.geometry)
+    filtered.forEach(disaster => {
+      const { geometry: { coordinates } } = disaster 
+      const polygon = turf.polygon(coordinates)
       const usersToAdd = []
       
       users.forEach( user => {
         const { location : { latitude, longitude } } = user
-        const where = classifyPoint(coords, [ +longitude, +latitude ]) 
-        // const isInside = getIsPointInsidePolygon([ +longitude, +latitude], coords)
-        console.log(where)
-        if(where < 1) {
+        const point = turf.point([+latitude, +longitude])
+        const isInside = inPolygon.default(point, polygon) 
+        if(isInside) {
           usersToAdd.push(user)
         }
       })
-      processed.push({event, 'nearbyUsers': usersToAdd})
+      const { properties : { 
+        severity,
+        event,
+        headline,
+        description,
+        instruction,
+        response
+       }} = disaster
+      processed.push({ 
+        severity,
+        event,
+        headline,
+        description,
+        instruction,
+        response,
+        'nearByUsers' : usersToAdd})
     })
-    // console.log(processed)
-
-    /*
-      LOOK UP RANDOM COORDINATE GENERATOR TO TEST IF THIS ALGORITHM ACTUALLY WORKS
-    */
-    return res.send(processed) 
+    return res.send(processed)
 }
 
 module.exports = listEvents
--- src/db/seeds/01_users.js
+++ src/db/seeds/01_users.js
@@ -7,8 +7,8 @@ exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('users').del()
   await knex('users').insert([
-    {username: 'nick', password_hash: 'yessir', latitude: '-104.3323517', longitude: '11.-104.3323517', is_safe: true},
-    {username: 'vick', password_hash: 'bessir', latitude: '85.5', longitude: '25.4', is_safe: true},
+    {username: 'nick', password_hash: 'yessir', latitude: '-84.187392', longitude: '35.7266481', is_safe: true},
+    {username: 'vick', password_hash: 'bessir', latitude: '-103.3858109', longitude: '40.4451181', is_safe: true},
     {username: 'sick', password_hash: 'lessir', latitude: '143.8', longitude: '21', is_safe: true},
     {username: 'kick', password_hash: 'qessir', latitude: '73', longitude: '42', is_safe: true},
     {username: 'rick', password_hash: 'essir', latitude: '118', longitude: '34', is_safe: true},
