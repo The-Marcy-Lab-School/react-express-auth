2a0c82fd81c97f66906f92b44183db8333cb46b9
luis <lrivera2022@amsbronx.org>
2023-06-26 11:15:33 -0400

    fixed nav bar

--- frontend/src/components/SiteHeadingAndNav.jsx
+++ frontend/src/components/SiteHeadingAndNav.jsx
@@ -4,6 +4,7 @@ import CurrentUserContext from "../contexts/current-user-context";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import NavItem from 'react-bootstrap/NavItem'
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
 
@@ -17,18 +18,23 @@ return (
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="me-auto">
-
-      <Nav.Link><NavLink to='/'>Home</NavLink></Nav.Link>
-      <Nav.Link><NavLink to='/about'>About</NavLink></Nav.Link>
+    {/* <NavLink to='/'>Home</NavLink> */}
+    {/* <NavItem to='/'>Home</NavItem> */}
+      <NavLink to='/'>Home </NavLink>
+      {/* <NavLink to='/about'>About</NavLink> */}
+      <NavLink to='/about'> About</NavLink>
       
       {
         currentUser ?
         <>
-         <Nav.Link><NavLink to='/susu'>Susu Pages</NavLink></Nav.Link>
+        {/* <NavLink to='/susu'>Susu Pages</NavLink> */}
+         <NavLink to='/susu'>Susu</NavLink>
          
          <NavDropdown title={currentUser.username} id="basic-nav-dropdown">
-         <Nav.Link><NavLink to='/createSusu' end={true}>Create/join Susu</NavLink></Nav.Link>
-         <Nav.Link><NavLink to={`/users/${currentUser.id}`}>{currentUser.username}</NavLink></Nav.Link>
+         {/* <NavLink to='/createSusu' end={true}>Create/join Susu</NavLink> */}
+         <NavLink to='/createSusu' >Create/Join Susu</NavLink>
+         {/* <NavLink to={`/users/${currentUser.id}`}>{currentUser.username}</NavLink> */}
+         <NavLink to={`/users/${currentUser.id}`}>{currentUser.username}</NavLink>
           
          </NavDropdown>
         </>
@@ -36,10 +42,12 @@ return (
         
         <> 
         <NavDropdown title="Join"id="basic-nav-dropdown">
-        <NavDropdown.Item><NavLink to='/sign-up'>Sign Up</NavLink></NavDropdown.Item>
-        <NavDropdown.Item>
+        {/* <NavLink to='/sign-up'>Sign Up</NavLink> */}
+        <NavLink to='/sign-up'>Sign Up</NavLink>
+        {/* <NavLink> */}
+        
         <NavLink to='/login'>Login</NavLink>
-        </NavDropdown.Item>
+        {/* </NavDropdown.Item> */}
         </NavDropdown>
         </>
       }
