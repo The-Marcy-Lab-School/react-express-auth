84459118416d3ece77c4d992cc98f67bcac685a8
luisc8487 <lf9904@gmail.com>
2023-06-26 14:22:59 -0400

    refactored novascore and nutrigrade

--- frontend/src/components/NutriScoreGrade.jsx
+++ frontend/src/components/NutriScoreGrade.jsx
@@ -5,59 +5,35 @@ import NutriGradeD from "../media/scores/nutriscore-d.svg";
 import NutriGradeE from "../media/scores/nutriscore-e.svg";
 import NutriNotApplicable from "../media/scores/nutriscore-not-applicable.svg";
 
-export default function NutriScoreGrade({ props }) {
+export default function NutriScoreGrade({ props: nutriGrade }) {
   return (
-    <>
-      {props === 'a' ? (
-        <>
-          <img
-            alt="Nova Score 1"
-            src={NutriGradeA}
-            style={{ width: "34px", height: "65px" }}
-          />
-        </>
-      ) : props === 'b' ? (
-        <>
-          <img
-            alt="Nova Score 2"
-            src={NutriGradeB}
-            // ../src/media/scores/nova-score2.svg
-            style={{ width: "34px", height: "65px" }}
-          />
-        </>
-      ) : props === "c" ? (
-        <>
-          <img
-            alt="Nova Score 3"
-            src={NutriGradeC}
-            style={{ width: "34px", height: "65px" }}
-          />
-        </>
-      ) : props === "d" ? (
-        <>
-          <img
-            alt="Nova Score 4"
-            src={NutriGradeD}
-            style={{ width: "34px", height: "65px" }}
-          />
-        </>
-      ) : props === "e" ? (
-        <>
-          <img
-            alt="Nova Score Unknown"
-            src={NutriGradeE}
-            style={{ width: "34px", height: "65px" }}
-          />
-        </>
-      ):(
-        <>
-        <img
-            alt="Nova Score Unknown"
-            src={NutriNotApplicable}
-            style={{ width: "34px", height: "65px" }}
-          />
-        </>
-      )}
-    </>
+    <div className="ui segment">
+      <div className="ui three column grid">
+        <div className="row">
+          <div className="column">
+            <strong>
+              <img
+                alt={`Nova Score ${nutriGrade}A}`}
+                src={
+                  nutriGrade === "a"
+                    ? NutriGradeA
+                    : nutriGrade === "b"
+                    ? NutriGradeB
+                    : nutriGrade === "c"
+                    ? NutriGradeC
+                    : nutriGrade === "d"
+                    ? NutriGradeD
+                    : nutriGrade === "e"
+                    ? NutriGradeE
+                    : NutriNotApplicable
+                }
+                style={{ width: "120px", height: "65px" }}
+              />
+            </strong>
+            <p>{nutriGrade === "a" ? "Very good nutritional quality" : nutriGrade === "b" ? "Good nutritional quality" : nutriGrade === "c" ? "Average nutritional quality" : nutriGrade === "d" ? "Poor nutritional quality" : nutriGrade === "e" ? "Bad nutritional quality" : "Unknown"}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
