b535509ef876f3762894bed8344ed03598af9faf
arlyy15 <raymundoarly15@gmail.com>
2023-06-28 12:56:26 -0400

    working on about us page

--- frontend/src/pages/AboutUs.jsx
+++ frontend/src/pages/AboutUs.jsx
@@ -1,5 +1,38 @@
 export default function AboutUs() {
   return <>
-    <h1>About Us Page</h1>
+  <div id = "mainAB">
+    <div id="header-Ab"><h1>About Us </h1></div>
+    <div id="body-Ab">
+      <h1>Our Mission</h1>
+      <p>jfndkjv</p>
+      <div id="image-Ab">
+      <img id = "image-Abp"src="https://images.thequint.com/thequint%2F2020-02%2F8b12865d-24e8-4d7a-9e48-580ff3f970df%2FFruits_and_Vegetables_QuintFit.jpg" alt=""></img>
+      </div>
+    </div>
+    <div id="team">
+      <h1>MEET OUR TEAM</h1>
+      <div id="team-images">
+        <div id="person1">
+          <img src="pic_trulli.jpg" alt=" "></img>
+          <p>hdehfhfh</p>
+        </div>
+        <div id="person2">
+          <img src="pic_trulli.jpg" alt=""></img>
+          <p>hdehfhfh</p>
+        </div>
+        <div id="person3">
+          <img src="pic_trulli.jpg" alt=""></img>
+          <p>hdehfhfh</p>
+        </div>
+        <div id="person4">
+          <img src="pic_trulli.jpg" alt=""></img>
+          <p>hdehfhfh</p>
+        </div>
+
+
+
+      </div>
+    </div>
+    </div>
   </>;
 }
