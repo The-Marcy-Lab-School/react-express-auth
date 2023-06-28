3c2dac452ced2097754bdc6ce542485c9cf20efa
“Xhesika” <xhesikam2184@tywls.org>
2023-06-28 13:27:21 -0400

    edits to index.js and knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
 const path = require('path');
 
+const migrationsDirectory = path.join(__dirname, 'src', 'db', 'migrations');
+const migrationsStub = path.join(__dirname, 'migration-stub.js');
+const seedsDirectory = path.join(__dirname, 'src', 'db', 'seeds');
+
 module.exports = {
   development: {
     client: 'pg',
@@ -8,15 +12,15 @@ module.exports = {
       host: process.env.PG_HOST || '127.0.0.1',
       port: process.env.PG_PORT || 5432,
       user: process.env.PG_USER || 'postgres',
-      password: process.env.PG_PASS || 'Waffles',
-      database: process.env.PG_DB || 'iqfood',
+      password: process.env.PG_PASS || 'postgres',
+      database: process.env.PG_DB || 'react_auth_example',
     },
     migrations: {
-      directory: path.join(__dirname, 'src', 'db', 'migrations'),
-      stub: path.join(__dirname, 'migration-stub.js'),
+      directory: migrationsDirectory,
+      stub: migrationsStub,
     },
     seeds: {
-      directory: path.join(__dirname, 'src', 'db', 'seeds'),
+      directory: seedsDirectory,
     },
   },
   test: {
@@ -27,6 +31,17 @@ module.exports = {
       user: process.env.PG_USER || 'postgres',
       password: process.env.PG_PASS || 'postgres',
       database: process.env.database || 'postgres',
+    },
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.PG_CONNECTION_STRING,
+    migrations: {
+      directory: migrationsDirectory,
+      stub: migrationsStub,
+    },
+    seeds: {
+      directory: seedsDirectory,
     },
   },
 };
