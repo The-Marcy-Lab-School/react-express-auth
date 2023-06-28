2e890ac0f529d161e2ecd69065697a71b26828a2
arlyy15 <raymundoarly15@gmail.com>
2023-06-28 16:11:31 -0400

    edited about us pg

--- frontend/src/pages/AboutUs.jsx
+++ frontend/src/pages/AboutUs.jsx
@@ -1,32 +1,39 @@
 export default function AboutUs() {
   return <>
   <div id = "mainAB">
-    <div id="header-Ab"><h1>About Us </h1></div>
+    <div id="header-Ab"><h1>About Us </h1>
+    <p id = "p1">iQFood is a place where the general public can be well-informed about the effects of processed foods one consumes and their impact on our bodies. With this information, individuals will be educated and aware of how much damage processed foods can do to our bodies. With that people will read about how they can make better decisions and create better dietary choices and meal plans to help with their health. While investing in unprocessed foods may appear financially burdensome, refraining from formulating meal plans or persisting with processed food consumption can incur substantial expenses over an extended period of time. iQFood increases awareness and informed decision-making contributing to positive dietary shifts and ultimately promoting a healthier population overall.</p>
+    </div>
     <div id="body-Ab">
       <h1>Our Mission</h1>
-      <p>jfndkjv</p>
+      <p id = "p2">iQFood is a platform created in response to the numerous reports of individuals who have digestive issues that ultimately lead to cancer and a greater risk of passing away. By educating the public on the dangerous chemicals present in our food, identifying healthier alternatives, and helping to create grocery lists, our users can achieve a longer life span.</p>
       <div id="image-Ab">
       <img id = "image-Abp"src="https://images.thequint.com/thequint%2F2020-02%2F8b12865d-24e8-4d7a-9e48-580ff3f970df%2FFruits_and_Vegetables_QuintFit.jpg" alt=""></img>
       </div>
+
     </div>
     <div id="team">
-      <h1>MEET OUR TEAM</h1>
-      <div id="team-images">
-        <div id="person1">
-          <img src="pic_trulli.jpg" alt=" "></img>
-          <p>hdehfhfh</p>
-        </div>
-        <div id="person2">
+      <h1 id = "teamTitle">MEET OUR TEAM</h1>
+      <div class="team-container">
+        <div class="team-member">
           <img src="pic_trulli.jpg" alt=""></img>
-          <p>hdehfhfh</p>
+         <h3>Luis Cordero</h3>
+          <p>Product Manger & Developer</p>
         </div>
-        <div id="person3">
+        <div class="team-member">
           <img src="pic_trulli.jpg" alt=""></img>
-          <p>hdehfhfh</p>
+          <h3>Xhesika Ma</h3>
+          <p>Scrum Master & Developer</p>
         </div>
-        <div id="person4">
+        <div class="team-member">
           <img src="pic_trulli.jpg" alt=""></img>
-          <p>hdehfhfh</p>
+          <h3>Isaac Sagesse</h3>
+          <p>Designer & Front-end Developer</p>
+        </div>
+        <div class="team-member">
+          <img src="pic_trulli.jpg" alt=""></img>
+          <h3>Arly Raymundo</h3>
+          <p>Back-end Developer</p>
         </div>
 
 
