8ac271dd83b494e4a34042027a9a4eb768682d07
treyjaded <trevonjcharles@gmail.com>
2023-06-29 15:05:13 -0400

    Added popup feed & style user id page

--- frontend/src/components/Events.jsx
+++ frontend/src/components/Events.jsx
@@ -4,12 +4,16 @@
 /* eslint-disable func-style */
 import React, { useEffect, useState, useContext } from 'react';
 import CurrentUserContext from '../contexts/current-user-context.js';
-import { apiFetchHandler } from '../utils';
+import DisasterPopupMain from './UserFeed.jsx';
+import { apiFetchHandler } from '../utils.js';
 
 // ------------------List of Events ----------------
 function EventList() {
   const [events, setEvents] = useState([]);
-  const { updateEventData, userLocation } = useContext(CurrentUserContext);
+  const [alert, setAlert] = useState([]);
+  const [userAlert, setUserAlert] = useState([]);
+  const { updateEventData, updateAlertData, userLocation, updateUserAlertData, setUserAlertData, currentUser } = useContext(CurrentUserContext);
+
   const fetchEvents = () => {
     fetch('https://eonet.gsfc.nasa.gov/api/v3/events?status=open&limit=20')
       .then((response) => response.json())
@@ -28,37 +32,30 @@ function EventList() {
 
   const fetchProcessed = async () => {
     const data = await apiFetchHandler('/api/events');
-    console.log("NICK API:", data);
+    setAlert(data[0]);
+    updateAlertData(data[0]);
+  };
+
+  const latitude = events[0]?.geometry[0]?.coordinates[0];
+  const longitude = events[0]?.geometry[0]?.coordinates[1];
+
+  const fetchLayers = () => {
+    fetch(`https://api.weather.gov/alerts?point=${userLocation?.myLatitude},${userLocation?.myLongitude}`)
+      .then((response) => response?.json())
+      .then((data) => {
+        setUserAlert(data);
+        updateUserAlertData(data);
+      })
+      .catch((error) => console.log(error));
   };
 
   useEffect(() => {
     fetchEvents();
     fetchProcessed();
     updateEventData();
+    updateUserAlertData();
+    fetchLayers();
   }, []);
-
-  function AlertList() {
-    const [alert, setAlert] = useState([]);
-
-    const latitude = events[0]?.geometry[0]?.coordinates[0];
-    const longitude = events[0]?.geometry[0]?.coordinates[1];
-
-    const fetchLayers = () => {
-      fetch(`https://api.weather.gov/alerts?point=${userLocation?.myLatitude},${userLocation?.myLongitude}`)
-        .then((response) =>
-          // console.log(response);
-          response.json())
-        .then((data) => {
-          setAlert(data.events);
-          // console.log("ALERT DATA:", alert);
-        })
-        .catch((error) => console.log(error));
-    };
-
-    useEffect(() => {
-      fetchLayers();
-    }, []);
-  }
 
   const latitude2 = 80;
   const latitude1 = 75;
@@ -74,41 +71,43 @@ function EventList() {
     );
     return math >= 4.5 ? `${Math.ceil(math)} miles` : `${Math.floor(math)} miles`;
   };
+  const eventId = events.map((event) => event.id);
 
   return (
-    <dl className="eventList">
-      <AlertList />
-      {events.map((event) => (
-        <React.Fragment key={event.id}>
-          <li>
-            <a href="#">
-              <div className="eventRow">
-                <div className="date">{event.geometry[0].date.slice(0, 16).replace("T", " ")}</div>
-                {event.categories.map((category) => (
-                  <div className="eventType" key={category.id}>
-                    {category.title}
+    <>
+      <dl className="eventList">
+        {events.map((event) => (
+          <React.Fragment key={event.id}>
+            <li>
+              <a href="#">
+                <div className="eventRow">
+                  <div className="date">{event.geometry[0].date.slice(0, 16).replace("T", " ")}</div>
+                  {event.categories.map((category) => (
+                    <div className="eventType" key={category.id}>
+                      {category.title}
+                    </div>
+                  ))}
+                  <div className="eventTitle">{event.title}</div>
+                  <div className="distance">
+                    {convertToMiles(
+                      latitude2,
+                      latitude1,
+                      longtitude2,
+                      longtitude1,
+                    )}
                   </div>
-                ))}
-                <div className="eventTitle">{event.title}</div>
-                <div className="distance">
-                  {convertToMiles(
-                    latitude2,
-                    latitude1,
-                    longtitude2,
-                    longtitude1,
-                  )}
                 </div>
-              </div>
-              {event.description && (
-                <dd>
-                  <em>{event.description}</em>
-                </dd>
-              )}
-            </a>
-          </li>
-        </React.Fragment>
-      ))}
-    </dl>
+                {event.description && (
+                  <dd>
+                    <em>{event.description}</em>
+                  </dd>
+                )}
+              </a>
+            </li>
+          </React.Fragment>
+        ))}
+      </dl>
+    </>
   );
 }
 
