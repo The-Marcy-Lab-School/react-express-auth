ad3d55ef3d6db509461464e15ac05083aab7d4a4
Destiny <dhowell831@gmail.com>
2023-06-29 12:46:32 -0400

    styling event form

--- frontend/src/pages/EventsPage.jsx
+++ frontend/src/pages/EventsPage.jsx
@@ -144,88 +144,92 @@ const handleRSVPClick = (event_id) => {
       <h1 id="events-header">Events</h1>
       <button id='make-event-btn' onClick={toggleFormVisibility}>Make Event</button>
       {showForm && (
-        <Form onSubmit={handleFormSubmit}>
-        <h1 id="events-form-header">Make an Event</h1>
-        <FormGroup>
-          <Label >Heading</Label>
-          <Input
-          type="text"
-          bsSize="lg"
-          id="heading-input"
-          name="heading-input"
-          placeholder="Heading"
-          className="mb-3"
-          value={eventHeader}
-          onChange={(e) => setEventHeader(e.target.value)}
-          />
-        </FormGroup>
-        <FormGroup>
-            <Label for="exampleFile">
-            Upload a Pic
-            </Label>
-            <Input
-            id="exampleFile"
-            name="file"
-            type="text"
-            value={ imgUrl }
-            onChange={(e) => setImgUrl(e.target.value)}
-            /> 
-        </FormGroup>
-        <FormGroup>
-            <Label for="exampleText">
-            Description
-            </Label>
-            <Input
-            id="exampleText"
-            name="text"
-            type="textarea"
-            value={ eventDescription }
-            onChange={(e) => setEventDescription(e.target.value)}
-            />
-        </FormGroup>
-        <FormGroup>
-            <Label for="exampleDate">
-            Date
-            </Label>
-            <Input
-            id="exampleDate"
-            name="date"
-            placeholder="date placeholder"
-            type="date"
-            value={ eventDate }
-            onChange={(e) => setEventDate(e.target.value)}
-            />
-        </FormGroup>
-        <FormGroup>
-            <Label for="exampleTime">
-            Time
-            </Label>
-            <Input
-            id="exampleTime"
-            name="time"
-            placeholder="time placeholder"
-            type="time"
-            value={ eventTime }
-            onChange={(e) => setEventTime(e.target.value)}
-            />
-        </FormGroup>
-        <FormGroup>
-          <Label for="exampleAddress">
-            Location
-          </Label>
-          <Input
-            id="exampleAddress"
-            name="address"
-            placeholder="1234 Main St"
-            type="text"
-            value={ location }
-            onChange={(e) => setLocation(e.target.value)}
-          />
-        </FormGroup>
-        <Button color="success">
-            Create Event
-        </Button>
-        </Form>
+        <div id="event-form">
+          <Card>
+            <Form onSubmit={handleFormSubmit}>
+              <h1 id="events-form-header">Make an Event</h1>
+              <FormGroup>
+                <Label >Heading</Label>
+                <Input
+                type="text"
+                bsSize="lg"
+                id="heading-input"
+                name="heading-input"
+                placeholder="Heading"
+                className="mb-3"
+                value={eventHeader}
+                onChange={(e) => setEventHeader(e.target.value)}
+                />
+              </FormGroup>
+              <FormGroup>
+                  <Label for="exampleFile">
+                  Upload a Pic
+                  </Label>
+                  <Input
+                  id="exampleFile"
+                  name="file"
+                  type="text"
+                  value={ imgUrl }
+                  onChange={(e) => setImgUrl(e.target.value)}
+                  /> 
+              </FormGroup>
+              <FormGroup>
+                  <Label for="exampleText">
+                  Description
+                  </Label>
+                  <Input
+                  id="exampleText"
+                  name="text"
+                  type="textarea"
+                  value={ eventDescription }
+                  onChange={(e) => setEventDescription(e.target.value)}
+                  />
+              </FormGroup>
+              <FormGroup>
+                  <Label for="exampleDate">
+                  Date
+                  </Label>
+                  <Input
+                  id="exampleDate"
+                  name="date"
+                  placeholder="date placeholder"
+                  type="date"
+                  value={ eventDate }
+                  onChange={(e) => setEventDate(e.target.value)}
+                  />
+              </FormGroup>
+              <FormGroup>
+                  <Label for="exampleTime">
+                  Time
+                  </Label>
+                  <Input
+                  id="exampleTime"
+                  name="time"
+                  placeholder="time placeholder"
+                  type="time"
+                  value={ eventTime }
+                  onChange={(e) => setEventTime(e.target.value)}
+                  />
+              </FormGroup>
+              <FormGroup>
+                <Label for="exampleAddress">
+                  Location
+                </Label>
+                <Input
+                  id="exampleAddress"
+                  name="address"
+                  placeholder="1234 Main St"
+                  type="text"
+                  value={ location }
+                  onChange={(e) => setLocation(e.target.value)}
+                />
+              </FormGroup>
+              <Button color="success">
+                  Create Event
+              </Button>
+            </Form>
+          </Card>
+        </div>
       )}
       {events.length === 0 ? (
         <p>No events available.</p>
