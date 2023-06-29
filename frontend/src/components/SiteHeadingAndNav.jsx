52903dac9b5b1cc143d9704bbc9795efbd8ba3ae
KristenSantos <Kristensxntos@gmail.com>
2023-06-29 07:01:08 -0400

    fixed form label color

--- frontend/src/components/SiteHeadingAndNav.jsx
+++ frontend/src/components/SiteHeadingAndNav.jsx
@@ -3,8 +3,6 @@ import { useContext, useState } from "react";
 import CurrentUserContext from "../contexts/current-user-context";
 
 
-
-
 export default function SiteHeadingAndNav() {
   const { currentUser } = useContext(CurrentUserContext);
 
