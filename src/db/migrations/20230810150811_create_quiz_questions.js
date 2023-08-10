bf589d751479d87850a8755ae69273971e7df8a9
Duojay Williams <dhwilliams3007@gmail.com>
2023-08-10 08:20:13 -0700

    made migratons

---
+++ src/db/migrations/20230810150811_create_quiz_questions.js
@@ -0,0 +1,25 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = (knex) => {
+    return knex.schema.createTable('quiz_questions', (table) => {
+        table.increments();
+        table.string('question').notNullable();
+        table.string('answer').notNullable();
+        table.string('wrong_answer_1').notNullable();
+        table.string('wrong_answer_2').notNullable();   
+        table.string('wrong_answer_3').notNullable();
+        table.integer('quiz_id').notNullable();
+        table.foreign('quiz_id').references('id').inTable('quiz_topic');
+        table.timestamps(true, true);
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = (knex) => {
+    return knex.schema.dropTable('quiz_questions');
+};
