ce875a192c148acf4c6f2ca3be9564345f68f104
Duojay Williams <dhwilliams3007@gmail.com>
2023-08-14 09:41:57 -0700

    fetch questions worked

--- frontend/src/pages/QuizLesson.jsx
+++ frontend/src/pages/QuizLesson.jsx
@@ -174,11 +174,54 @@ function UserList() {
   
   console.log("questions", questions); // Use a comma to separate the string and the variable  
 
-  return (
-    <div>
+
+
+
+//   // return (
+//   //   <div className="quiz-test">
+//   //     {/* 1. Header */}
+//   //     <h1> Spanish Quiz</h1>
+      
+//   //     {/* 2. Current Score */}
+//   //     <h2> Current Score:{score}</h2>
+      
+//   //     {
+//   //      {showFinalResults ? (
+//   //        Final Results
+//   //       <div className="final-results">
+//   //         <h1>Final Results</h1>
+//   //         <h2>1 out of 5 correct - (30%)</h2>
+//   //         <button>Restart Quiz</button>
+//   //       </div>
+//   //     ) : (
+//   //        Question Card
+//   //       <div className="question-card">
+//   //         <h2>Question {currentQuestion + 1} out of {questions.length}</h2>
+//   //         <h3 className="question-text">{questions[currentQuestion].text}</h3>
+//   //         <ul>
+//   //           {{questions[currentQuestion].options.map((option) => {
+//   //             return (
+//   //               <li
+//   //                 key={option.id}
+//   //                 onClick={() => optionClicked(option.isCorrect)}
+//   //               >
+//   //                 {option.text}
+//   //               </li>
+//   //             );
+//   //           })} }
+//   //         { </ul>
+//   //       </div>
+//   //     )} 
+//   //      } 
+      
+//   //   </div>
+//   // );
+
+  // return (
+  //   <div>
    
-    </div>
-  );
+  //   </div>
+  // );
 }
 
 export default UserList;
