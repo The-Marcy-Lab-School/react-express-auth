4057651d4e94f94f375f2c93b51c66cefb495a1c
Staceyannking <staceyannking01@gmail.com>
2023-08-19 16:21:50 -0400

    Got quiz to shuffle the answer from the database

--- frontend/src/pages/QuizTestPage.jsx
+++ frontend/src/pages/QuizTestPage.jsx
@@ -143,23 +143,39 @@
 //   );
 // }
 
+
+
 import React, { useState, useEffect } from "react";
+
+function shuffleArray(array) {
+  return array.slice().sort(() => Math.random() - 0.5);
+}
 
 export default function QuizTestPage() {
   const [showFinalResults, setFinalResults] = useState(false);
   const [score, setScore] = useState(0);
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [questions, setQuestions] = useState([]);
-  const [selectedOption, setSelectedOption] = useState(null); // New state for tracking selected option
+  const [selectedOption, setSelectedOption] = useState(null);
   const [wrongAnswersList, setWrongAnswersList] = useState([]);
 
-  // This is fectching questions form the database
   useEffect(() => {
     fetch('api/questions')
       .then(response => response.json())
       .then(data => {
         console.log("question data", data);
-        setQuestions(data); 
+
+        const questionsWithShuffledOptions = data.map(question => ({
+          ...question,
+          options: shuffleArray([
+            question.answer,
+            question.wrong_answer_1,
+            question.wrong_answer_2,
+            question.wrong_answer_3,
+          ]),
+        }));
+
+        setQuestions(questionsWithShuffledOptions);
       })
       .catch(error => console.error(error));
   }, []);
@@ -168,18 +184,18 @@ export default function QuizTestPage() {
     const correctAnswer = questions[currentQuestion].answer;
 
     if (selectedAnswer !== correctAnswer) {
-      setWrongAnswersList((prevList) => [...prevList, selectedAnswer]); // Add the selected wrong answer to the list
+      setWrongAnswersList((prevList) => [...prevList, selectedAnswer]);
     }
 
     if (selectedAnswer === correctAnswer) {
       setScore(score + 1);
     } else {
-      setSelectedOption(selectedAnswer); // Store the selected wrong answer
+      setSelectedOption(selectedAnswer);
     }
 
     if (currentQuestion + 1 < questions.length) {
       setCurrentQuestion(currentQuestion + 1);
-      setSelectedOption(null); // Clear the selected wrong answer when moving to the next question
+      setSelectedOption(null);
     } else {
       setFinalResults(true);
     }
@@ -200,14 +216,14 @@ export default function QuizTestPage() {
         <div className="final-results">
           <h1>Final Results</h1>
           <h2>{score} out of {questions.length} correct - ({(score / questions.length) * 100}%)</h2>
-                  <div>
-              <h3>Wrong Answers:</h3>
-              <ul>
-                {wrongAnswersList.map((wrongAnswer, index) => (
-                  <li key={index}>{wrongAnswer}</li>
-                ))}
-              </ul>
-            </div>
+          <div>
+            <h3>Wrong Answers:</h3>
+            <ul>
+              {wrongAnswersList.map((wrongAnswer, index) => (
+                <li key={index}>{wrongAnswer}</li>
+              ))}
+            </ul>
+          </div>
           <button onClick={restartQuiz}>Restart Quiz</button>
         </div>
       ) : (
@@ -215,35 +231,28 @@ export default function QuizTestPage() {
           <h2>Question {currentQuestion + 1} out of {questions.length}</h2>
           <h3 className="question-text">{questions[currentQuestion]?.question}</h3>
           <ul>
-            <li
-              onClick={() => optionClicked(questions[currentQuestion]?.wrong_answer_1)}
-              className={selectedOption === questions[currentQuestion]?.wrong_answer_1 ? "selected-wrong" : ""}
-            >
-              {questions[currentQuestion]?.wrong_answer_1}
-            </li>
-            <li
-              onClick={() => optionClicked(questions[currentQuestion]?.wrong_answer_2)}
-              className={selectedOption === questions[currentQuestion]?.wrong_answer_2 ? "selected-wrong" : ""}
-            >
-              {questions[currentQuestion]?.wrong_answer_2}
-            </li>
-            <li
-              onClick={() => optionClicked(questions[currentQuestion]?.wrong_answer_3)}
-              className={selectedOption === questions[currentQuestion]?.wrong_answer_3 ? "selected-wrong" : ""}
-            >
-              {questions[currentQuestion]?.wrong_answer_3}
-            </li>
-            <li
-              onClick={() => optionClicked(questions[currentQuestion]?.answer)}
-              className={selectedOption === questions[currentQuestion]?.answer ? "selected-correct" : ""}
-            >
-              {questions[currentQuestion]?.answer}
-            </li>
+            {questions[currentQuestion]?.options.map((option, index) => (
+              <li
+                key={index}
+                onClick={() => optionClicked(option)}
+                className={
+                  selectedOption === option
+                    ? option === questions[currentQuestion]?.answer
+                      ? "selected-correct"
+                      : "selected-wrong"
+                    : ""
+                }
+              >
+                {option}
+              </li>
+            ))}
           </ul>
         </div>
       )}
     </div>
   );
 }
+
+
 
 
