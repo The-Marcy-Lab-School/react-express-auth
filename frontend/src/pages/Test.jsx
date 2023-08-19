2d6c1ee33f35c1d9155ae1265540fee543ded08b
Duojay Williams <dhwilliams3007@gmail.com>
2023-08-18 21:50:10 -0700

    fixed any  errors

--- frontend/src/pages/Test.jsx
+++ frontend/src/pages/Test.jsx
@@ -31,22 +31,75 @@
 // }
 
 // export default UserList;
-import React from "react";
-import "regenerator-runtime";
-import speech, { useSpeechRecognition } from "react-speech-recognition";
 
+
+
+////A.I TEST
+import React, { useEffect } from "react";
+import "regenerator-runtime/runtime"; // Import regenerator-runtime to support async/await
+import speech, { useSpeechRecognition } from "react-speech-recognition";
+const key = "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"
 function Test() {
   const { listening, transcript } = useSpeechRecognition();
 
+  async function callGpt3API(message) {
+    try {
+      const response = await fetch("https://api.openai.com/v1/chat/completions", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer  ${key}` // Replace with your actual API key
+        },
+        body: JSON.stringify({
+          messages: [{ role: "user", content: message }],
+          model: "gpt-3.5-turbo" // Update to the correct model name
+        })
+      });
+
+      const data = await response.json();
+      console.log(data)
+      return data;
+    } catch (error) {
+      console.error("Error:", error);
+      throw error;
+    }
+  }
+
+  useEffect(() => {
+    if (!listening && transcript) {
+      callGpt3API(transcript)
+        .then(response => {
+          const speechSynthesis = window.speechSynthesis;
+          const utterance = new SpeechSynthesisUtterance(response);
+          speechSynthesis.speak(utterance);
+        })
+        .catch(error => {
+          console.error("Error:", error);
+        });
+    }
+  }, [transcript, listening]);
+
   return (
-    <>
+    <div>
       {listening ? <p>wrk</p> : <p>CLICK</p>}
-      <button onClick={() => speech.startListening()}>Ask</button> 
-      {
-        transcript && < div> {transcript}</div>
-      }
-    </>
+      <button onClick={() => speech.startListening()}>Ask</button>
+      {transcript && <div>{transcript}</div>}
+    </div>
   );
 }
 
 export default Test;
+
+
+// import{Configuration, OpenAIAPI} from "openai"
+// const completion = new OpenAIAPI(new Configuration({
+//   apiKey: "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"
+// }));
+
+//  openai.createCompletion({
+//   model: "text-davinci-003",
+//   messages: [
+// {role:"user", content:"hello"}
+//   ]
+// })
+// .then (res => console.log(res))
