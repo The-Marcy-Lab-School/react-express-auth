24dec681f71e5531f6e31dd5dad29647ce0c649b
Duojay Williams <dhwilliams3007@gmail.com>
2023-08-26 11:36:16 -0700

    created discussion board table

---
+++ src/db/migrations/20230826180213_discussion_board.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable('discussion_boards', table => {
+    table.increments('id').primary();
+    table.string('topic').notNullable();
+    table.text('description').notNullable();
+    table.integer('user_id').unsigned().notNullable();
+    table.foreign('user_id').references('id').inTable('users');
+    table.timestamps(true, true);
+    });
+  };
+  
+  exports.down = function(knex) {
+    return knex.schema.dropTable('discussion_boards');
+  };
+  
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
