2eebe4faaed8de68d5e53f1956246715a8261012
MaiJGitHub <jmai112302@gmail.com>
2024-01-17 14:32:02 -0500

    migration made

---
+++ src/db/migrations/20240117174748_create_posts_table.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = (knex) => knex.schema.createTable('post', (table) => {
+    table.increments('post_id');
+    table.string('post_title').notNullable();
+    table.string('post_content').notNullable();
+    table.string('post_url').notNullable();
+    table.integer('user_id').references('user_id').inTable('users');
+    table.timestamps(true, true);
+  });
+  
+  /**
+   * @param { import("knex").Knex } knex
+   * @returns { Promise<void> }
+   */
+  exports.down = (knex) => knex.schema.dropTable('posts');
+  
