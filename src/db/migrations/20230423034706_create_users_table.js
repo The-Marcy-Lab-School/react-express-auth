abe34691a8f08744dcacd75b67841e5be3fe62bb
Angiegru <angiegrullon01@gmail.com>
2024-01-17 16:03:58 -0500

    Updated migration and created seeds

--- src/db/migrations/20230423034706_create_users_table.js
+++ src/db/migrations/20230423034706_create_users_table.js
@@ -9,8 +9,17 @@ exports.up = (knex) => knex.schema.createTable('users', (table) => {
   table.timestamps(true, true);
 });
 
+exports.up = (knex) => knex.schema.createTable('journal_entries', (table) => {
+  table.increments();
+  table.string('userId').not
+  table.string('content');
+  table.timestamps(true, true);
+  
+  table.foreign('journal_entries').references('user_id').inTable('users');
+});
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
 exports.down = (knex) => knex.schema.dropTable('users');
+exports.down = (knex) =>knex.schema.dropTable('journal_entries');
