d4a51b2a77d05643c98dc22fd15cf72497e506a7
ashleysimone1 <ashtucker1204@gmail.com>
2024-01-17 10:50:53 -0500

    completed backend

--- src/db/migrations/20240117174748_create_posts_table.js
+++ src/db/migrations/20240117174748_create_posts_table.js
@@ -7,7 +7,8 @@ exports.up = (knex) => knex.schema.createTable('post', (table) => {
     table.string('post_title').notNullable();
     table.string('post_content').notNullable();
     table.string('post_url').notNullable();
-    table.integer('user_id').references('user_id').inTable('users');
+    table.integer('user_id')
+    table.foreign('user_id').references('user_id').inTable('users');
     table.timestamps(true, true);
   });
   
