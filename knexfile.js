4fbf8d338302580dd5a0363afe94287096c2ab86
engineerErica <ericaponce2002@gmail.com>
2024-01-18 18:26:20 -0500

    failing to migrate the tasks table to my backend

--- knexfile.js
+++ knexfile.js
@@ -12,7 +12,7 @@ module.exports = {
       host: process.env.PG_HOST || '127.0.0.1',
       port: process.env.PG_PORT || 5432,
       user: process.env.PG_USER || 'postgres',
-      password: process.env.PG_PASS || 'postgres',
+      password: process.env.PG_PASS || '123',
       database: process.env.PG_DB || 'react_auth_example',
     },
     migrations: {
