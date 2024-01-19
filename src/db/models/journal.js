586ef189c0b05b2ff8296343d9fed3a6b349d8a8
Angiegru <angiegrullon01@gmail.com>
2024-01-19 18:22:22 -0500

    Updated

--- src/db/models/journal.js
+++ src/db/models/journal.js
@@ -1,36 +1,66 @@
 const knex = require('../knex');
 
 class JournalEntry {
-    constructor({ id, userId, date, content }) {
-        this.id = id;
-        this.userId = userId;
-        this.date = date;
-        this.content = content;
-    }
+    static async getAllJournalEntries(){
+        try{
+            const query = 'SELECT * FROM users JOIN journal_entries ON users.id = journal_entries.user_id';
+            const { rows } = await knex.raw(query, args);
+            return rows;
+            } catch (err) {
+                console.log(err);
+                return null;
+            }
+        }
+    
+        static async getJournalEntryById(id){
 
-    static async create(userId, date, content) {
+        }
+    static async getJournalById(id) {
+        try {
+            const query = 'SELECT * FROM users JOIN journal_entries ON users.id = journal_entries.user_id WHERE users.id = ?';
+            const { rows } = await knex.raw(query, args);
+            return rows;
+            } catch (err) {
+                console.log(err);
+                return null;
+            }
+        }
+
+    static async addJournalEntry(userId, date, content) {
+        try{
         const query = `INSERT INTO journal_entries (user_id, date, content) VALUES (?, ?, ?) RETURNING *`;
         const args = [userId, date, content];
         const { rows } = await knex.raw(query, args);
-        const entry = rows[0];
-        return new JournalEntry(entry);
+        return rows[0];
+        } catch (err) {
+            console.log(err);
+            return null;
+        }
     }
 
     static async readJournalEntries(userId) {
+        try{
         const query = 'SELECT * FROM journal_entries WHERE user_id = ?';
         const args = [userId];
         const { rows } = await knex.raw(query, args);
-        return rows.map((entry) => new JournalEntry(entry))
+        return rows;
+        } catch (err) {
+            console.log(err);
+            return [];
+        }
     }
 
     static async delete(id) {
+        try{
         const query = 'Delete FROM journal_entries WHERE id = ? RETURNING *';
         const args = [id];
         const { rows } = await knex.raw(query, args);
-        const deletedEntry = rows[0];
-        return deletedEntry ? new JournalEntry(deletedEntry) : null;
+        return rows[0] || null;
+        } catch (err) {
+            console.log(err);
+            return null;
+        }
     }
-
 }
 
 module.exports = JournalEntry;
