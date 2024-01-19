5349ecc8cca29df1cf9814591c834463485657db
SirreaReeves <reevessirrea@gmail.com>
2024-01-18 19:46:33 -0500

    Seeds are running

--- src/db/migrations/20240117193612_moods.js
+++ src/db/migrations/20240117193612_moods.js
@@ -3,9 +3,11 @@
  * @returns { Promise<void> }
  */
 exports.up = (knex) => knex.schema.createTable('moods', (table) => {
-    table.increments().primary();
+    table.increments('id').primary();
     table.string('mood').notNullable();
     table.string('level').notNullable();
+
+    table.integer('user_id');
     table.foreign('user_id').references('id').inTable('users');
 });
 
