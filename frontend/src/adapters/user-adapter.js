b1cac371254efa50ea5ffbb3636f59f8d2835ebe
SamuelPickering <samuelpickering21@gmail.com>
2024-01-29 16:37:32 -0500

    Added a function to fetch all events from a user

--- frontend/src/adapters/user-adapter.js
+++ frontend/src/adapters/user-adapter.js
@@ -2,9 +2,21 @@ import { fetchHandler, getPostOptions, getPatchOptions } from "../utils";
 
 const baseUrl = '/api/users';
 
-export const createUser = async ({ username, password }) => (
-  fetchHandler(baseUrl, getPostOptions({ username, password }))
-);
+
+
+export const createUser = async ({ username, password, fullName, email }) => {
+  console.log(fullName)
+  const name = fullName
+  console.log(name, username)
+  const res = await fetchHandler(baseUrl, getPostOptions({ username, password, name, email }))
+  return res
+};
+
+export const fetchJoinedEvents = async (userId) => {
+  const [events] = await fetchHandler(`${baseUrl}/events/${userId}/signed`);
+  return events || [];
+};
+
 
 export const getAllUsers = async () => {
   const [users] = await fetchHandler(baseUrl);
@@ -16,3 +28,7 @@ export const getUser = async (id) => fetchHandler(`${baseUrl}/${id}`);
 export const updateUsername = async ({ id, username }) => (
   fetchHandler(`${baseUrl}/${id}`, getPatchOptions({ id, username }))
 );
+
+const app = ({
+
+})
