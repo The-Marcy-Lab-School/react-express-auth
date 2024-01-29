6bc49a00b01b62cc5c50fd93bc1f9fca2a031612
SamuelPickering <samuelpickering21@gmail.com>
2024-01-29 16:31:08 -0500

    Installed cors and server now uses cors middleware

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const handleCookieSessions = require('./middleware/handle-cookie-sessions');
 const router = require('./router');
 const logRoutes = require('./middleware/log-routes');
+const cors = require("cors")
 
 const app = express();
 
@@ -10,6 +11,8 @@ app.use(handleCookieSessions); // adds a session property to each request repres
 app.use(logRoutes); // print information about each incoming request
 app.use(express.json()); // parse incoming request bodies as JSON
 app.use(express.static(path.join(__dirname, '..', 'public'))); // Serve static assets from the public folder
+app.use(cors())
+
 
 app.use('/api', router);
 
