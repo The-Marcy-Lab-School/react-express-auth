1aded4e23ea7a3a79d964c39ed25710a24f9f28f
SamuelPickering <samuelpickering21@gmail.com>
2024-01-29 16:28:55 -0500

    Comment controller complete with all functions and barrel file

---
+++ src/controllers/comment/index.js
@@ -0,0 +1,12 @@
+const getCommentsOnEvent = require("./getCommentsOnEvent")
+const getCommentsByUser = require("./getCommentsByUser")
+const postComment = require("./postComment")
+
+
+module.exports = {
+    getCommentsOnEvent,
+    getCommentsByUser,
+    postComment,
+
+
+}
