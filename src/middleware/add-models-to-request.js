f52e330f82d0db699aa62d719812f250c8c7ede9
Joeyyyys <josephgiler2@gmail.com>
2024-01-31 15:33:56 -0500

    added Post and Comment models to the request so we can use the instance methods

--- src/middleware/add-models-to-request.js
+++ src/middleware/add-models-to-request.js
@@ -1,8 +1,14 @@
 const User = require('../db/models/user');
+const Post = require('../db/models/post');
+const Comment = require('../db/models/comment');
+const Likes = require('../db/models/like');
 
 const addModelsToRequest = (req, res, next) => {
   req.db = {
     User,
+    Post,
+    Comment,
+    
   };
   next();
 };
