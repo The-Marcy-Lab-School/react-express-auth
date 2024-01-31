b810955e2006744c423b2d903fe34456cb82332a
Joeyyyys <josephgiler2@gmail.com>
2024-01-30 20:03:29 -0500

    migrated a table into our SQL database called comments and seeded data into our row

---
+++ src/db/migrations/20240131005021_create_comments_table.js
@@ -0,0 +1,17 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = (knex) => knex.schema.createTable('comments', (table) => {
+    table.increments(); // Comment ID 
+    table.integer('user_id').references('id').inTable('users').notNullable().onDelete('CASCADE'); // foreign key that links to the id in users
+    table.integer('post_id').references('id').inTable('posts').notNullable().onDelete('CASCADE'); // foreign key that links to the id in posts
+    table.string('content').notNullable(); // content of comment
+    table.string('time'); // the time of creation of comment
+  });
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = (knex) => knex.schema.dropTable('comments');
---
+++ src/db/seeds/comment.js
@@ -0,0 +1,11 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> } 
+ */
+exports.seed = async function(knex) {
+  // Deletes ALL existing entries
+  await knex('comments').del()
+  await knex.table('comments').insert([
+    {user_id: 2, post_id: 1, content: 'This is an amazing post!', time: '8:00 PM' },
+  ]);
+};
