2a1579d637f9c8d2cf1c288a2729bb664a6b76c6
David <dcobos00@gmail.com>
2024-02-01 14:17:45 -0500

    finished componemts

--- frontend/src/components/NewsScroll.jsx
+++ frontend/src/components/NewsScroll.jsx
@@ -1,7 +1,10 @@
 import { useContext, useEffect, useState } from "react";
 
 export default function NewsScroll(){
+
+    let {useNews, news} = useEffect()
+
     return <>
-    
+        
     </>
 }
