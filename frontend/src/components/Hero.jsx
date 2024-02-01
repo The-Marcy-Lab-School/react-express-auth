46d3bab19b0d3b021ba1ab27cead6b6b4fceb811
gonzzoh <gmeza0118@gmail.com>
2024-02-01 15:30:22 -0500

    Footer

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import { useContext, useEffect, useState } from "react";
+import HeroImg from "../imgs/cosmic-trash.jpeg";
+
+export default function Hero(){
+    return <>
+        <section id="hero">
+            <img id="hero-img" src={HeroImg} alt="hero image" />
+            <container id="hero-text">
+                <h1>WE OUT HERE YESSIR</h1>
+                <p>Trash, Trash, Trash</p>
+            </container>
+        </section>
+    </>
+}
