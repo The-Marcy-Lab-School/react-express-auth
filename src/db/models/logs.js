f5eeedb992c6545ff5441790b78315616b937b3f
Domosayshello <dominique.arno@gmail.com>
2024-02-01 13:02:17 -0500

    contollers folders for entries and logs and editis to logs model

--- src/db/models/logs.js
+++ src/db/models/logs.js
@@ -1,52 +1,49 @@
 const knex = require('../knex');
 
 
-class Logs {
-    #passwordHash = null; // a private property
-
-    constructor({ id, userId, mood, abdominal_pain, backpain, nauseau, fatigue }) {
-        this.id = id;
-        this.userId = userId;
-        this.mood = mood;
-        this.abdominal_pain = abdominal_pain
-        this.backpain = backpain,
-            this.nauseau = nauseau,
-            this.nauseau = nauseau
-      
-      
+class Log {
+   
+  constructor({ id, userId, mood, abdominal_pain, backpain, nausea, fatigue }) {
+    this.id = id;
+    this.userId = userId;
+    this.mood = mood;
+    this.abdominal_pain = abdominal_pain;
+    this.backpain = backpain;
+    this.nauseau = nausea;
+    this.nauseau = fatigue;
     }
 
     static async list() {
         const query = 'SELECT * FROM logs';
         const { rows } = await knex.raw(query);
-        return rows.map((user) => new Logs(user));
+        return rows.map((log) => new Log(log));
     }
   
-    static async create(userId, mood, abdominal_pain, backpain, nauseau, fatigue) {
+    static async create(userId, mood, abdominal_pain, backpain, nausea, fatigue) {
         const query = `INSERT INTO logs (user_id, mood, abdominal_pain, backpain, nauseau, fatigue)
           VALUES (?, ?, ?, ?, ?, ?) RETURNING *`;
-        const args = [userId, mood, abdominal_pain, backpain, nauseau, fatigue];
+        const args = [userId, mood, abdominal_pain, backpain, nausea, fatigue];
         const { rows } = await knex.raw(query, args);
         const logEntry = rows[0];
         return logEntry; // Assuming you have a LogEntry class to handle the log object
     }
 
-    static async deleteAll() {
-        return knex.raw('TRUNCATE users;');
+    static async delete(logId) {
+        return knex.raw('DELETE FROM logs WHERE id = ?;');
     }
-    static async update(logId, userId, mood, abdominal_pain, backpain, nauseau, fatigue) {
+    static async update(logId, userId, mood, abdominal_pain, backpain, nausea, fatigue) {
         const query = `UPDATE logs SET 
           mood = ?,
           abdominal_pain = ?,
           backpain = ?,
-          nauseau = ?,
+          nausea = ?,
           fatigue = ?
           WHERE id = ? AND user_id = ? RETURNING *`;
-        const args = [mood, abdominal_pain, backpain, nauseau, fatigue, logId, userId];
+        const args = [mood, abdominal_pain, backpain, nausea, fatigue, logId, userId];
         const { rows } = await knex.raw(query, args);
         const updatedLogEntry = rows[0];
         return updatedLogEntry; // Assuming you have a LogEntry class to handle the log object
       }
 }
 
-module.exports = User;
+module.exports = Log;
