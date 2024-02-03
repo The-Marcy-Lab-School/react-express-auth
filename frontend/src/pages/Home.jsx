803e723dd1044da1d09fb59c8e8f6c0fecc32c05
thisischu <chuyilei1@gmail.com>
2024-02-03 14:50:35 -0500

    update front end component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@
 import Header from '../components/HomeComponents/HomeHeader';
 import Mission from '../components/HomeComponents/HomeMission';
 import Features from '../components/HomeComponents/HomeFeatures';
+import Articles from '../components/HomeComponents/HomeArticles';
 
 export default function HomePage() {
   return (
@@ -9,7 +10,7 @@ export default function HomePage() {
       <Header />
       <Mission />
       <Features />
-
+      <Articles />
     </>
   );
 }
