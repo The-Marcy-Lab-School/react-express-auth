31f195803cba4a315fbc6d57b01399a684e059ac
SamuelPickering <samuelpickering21@gmail.com>
2024-02-05 10:17:49 -0800

    Created socket.io server and on connection function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 require('dotenv').config();
 const server = require('./server');
+const io = require('./socketIoServer')
 
 const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
+
+const emailToSocketIdMap = new Map()
+const socketidToEmailMap = new Map()
+
+io.on('connection', socket => {
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId)
+    console.log(`${userId} in ${roomId}`)
+    socket.to(roomId).emit('user-connected',userId)
+    
+    socket.on('message', message => {
+      io.to(roomId).emit('createMessage', message)
+    })
+    socket.on('disconnect', () => {
+      socket.to(roomId).emit('user-disconnected', userId)
+    })
+  })
+})
