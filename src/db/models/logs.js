ce9295cd5f52aa2c45072ed83eab8453b4af9a10
Domosayshello <dominique.arno@gmail.com>
2024-02-04 20:55:57 -0500

    consistency in table architecture

--- src/db/models/logs.js
+++ src/db/models/logs.js
@@ -2,16 +2,6 @@ const knex = require('../knex');
 
 
 class Log {
-   
-  constructor({ id, userId, mood, abdominal_pain, backpain, nausea, fatigue }) {
-    this.id = id;
-    this.userId = userId;
-    this.mood = mood;
-    this.abdominal_pain = abdominal_pain;
-    this.backpain = backpain;
-    this.nauseau = nausea;
-    this.nauseau = fatigue;
-    }
 
     static async list() {
         const query = 'SELECT * FROM logs';
@@ -19,24 +9,24 @@ class Log {
         return rows.map((log) => new Log(log));
     }
   
-    static async create(userId, mood, abdominal_pain, backpain, nausea, fatigue) {
-        const query = `INSERT INTO logs (user_id, mood, abdominal_pain, backpain, nauseau, fatigue)
+    static async create(mood, abd_pain, back_pain, nausea, fatigue, user_id) {
+        const query = `INSERT INTO logs (mood, abd_pain, back_pain, nausea, fatigue, user_id, )
           VALUES (?, ?, ?, ?, ?, ?) RETURNING *`;
-        const args = [userId, mood, abdominal_pain, backpain, nausea, fatigue];
+        const args = [mood, abd_pain, back_pain, nausea, fatigue, user_id];
         const { rows } = await knex.raw(query, args);
         const logEntry = rows[0];
         return logEntry; // Assuming you have a LogEntry class to handle the log object
     }
 
-    static async update(logId, userId, mood, abdominal_pain, backpain, nausea, fatigue) {
+    static async update(mood, abd_pain, back_pain, nausea, fatigue, logId, user_id) {
         const query = `UPDATE logs SET 
           mood = ?,
-          abdominal_pain = ?,
-          backpain = ?,
+          abd_pain = ?,
+          back_pain = ?,
           nausea = ?,
           fatigue = ?
           WHERE id = ? AND user_id = ? RETURNING *`;
-        const args = [mood, abdominal_pain, backpain, nausea, fatigue, logId, userId];
+        const args = [ mood, abd_pain, back_pain, nausea, fatigue, logId, user_id];
         const { rows } = await knex.raw(query, args);
         const updatedLogEntry = rows[0];
         return updatedLogEntry; // Assuming you have a LogEntry class to handle the log object
