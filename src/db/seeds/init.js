ce963c66823beece7647aac340da4161a1708a10
thisischu <chuyilei1@gmail.com>
2024-02-05 14:44:46 -0500

    logs seeds added

--- src/db/seeds/init.js
+++ src/db/seeds/init.js
@@ -7,7 +7,7 @@ const logRoutes = require('../../middleware/log-routes');
  */
 exports.seed = async (knex) => {
   await knex('users').del()
-  await User.create(224, 'cool_cat', 'coolcat@gmail.com', '1234');
-  await User.create(323,'l33t-guy', 'l33t-guy@gmail.com', '1234');
-  await User.create(567, 'wowow', 'wowow@gmail.com', '1234');
+  await User.create('cool_cat', 'coolcat@gmail.com', '1234');
+  await User.create('l33t-guy', 'l33t-guy@gmail.com', '1234');
+  await User.create('wowow', 'wowow@gmail.com', '1234');
 };
