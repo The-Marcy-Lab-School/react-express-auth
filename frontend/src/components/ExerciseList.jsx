d4aa0f77a49c05f9e90e580727518485587f23aa
SamuelPickering <samuelpickering21@gmail.com>
2024-02-07 19:48:45 -0500

    State for selected exercise and exercise li becomes blue upon click

--- frontend/src/components/ExerciseList.jsx
+++ frontend/src/components/ExerciseList.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
+import { useState } from 'react'
 import "./styles/ExerciseList.css"
 const ExerciseList = (props) => {
     const {exercises} = props
+    const [ExerciseIndex, setExerciseIndex] = useState(null)
+    const handleExerciseClick = (index) => {
+        setExerciseIndex(index);
+    };
+
   return (
     <>
         <ul className='exercise-list'>
-        {exercises.map((exercise) => {
-         return (<li className='list-item' key={exercise.name + exercise.id}>{exercise.name}</li>)
+        {exercises.map((exercise, index) => {
+         return (<li className={`list-item ${ExerciseIndex === index ? 'active' : ''}`}
+         bindex={index} 
+         key={exercise.name + exercise.id}
+         onClick={() => handleExerciseClick(index)}>{exercise.name}
+         </li>)
     })}
     {exercises.length === 0 && "No exercises 😹"}
         </ul>
