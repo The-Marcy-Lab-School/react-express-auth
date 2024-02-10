f5f23b8dd9ba3341f85a79d5c37b4a84d5f465c9
gonzzoh <gmeza0118@gmail.com>
2024-02-09 21:40:39 -0500

    Updated Nav, User Page, and Posts Page

--- frontend/src/components/UserProfileCard.jsx
+++ frontend/src/components/UserProfileCard.jsx
@@ -1,14 +1,18 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import CurrentUserContext from "../contexts/current-user-context";
 import { logUserOut } from "../adapters/auth-adapter";
+import { getAllUserLikes } from "../adapters/like-adapter";
+import { getAllUserComments } from "../adapters/comment-adapter";
 
 const UserProfileCard = ({ username, bio, profileimage }) => {
   const navigate = useNavigate();
   const { currentUser, setCurrentUser } = useContext(CurrentUserContext);
   const { id } = useParams();
   const isCurrentUserProfile = currentUser && currentUser.id === Number(id);
-
+  const [clicked, setClicked] = useState("posts");
+  const [userLikes, setUserLikes] = useState([]);
+  const [userComments, setUserComments] = useState([]);
 
   const handleLogout = async () => {
     logUserOut(); // Call the 'logUserOut' function from the auth adapter
@@ -16,23 +20,72 @@ const UserProfileCard = ({ username, bio, profileimage }) => {
     navigate('/'); // Navigate to the home page
   };
 
+  const handleLikesClick = async (id) => {
+    setClicked("likes");
+    try {
+      const result = await getAllUserLikes(id);
+      setUserLikes(result);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handlePostsClick = async (id) => {
+    setClicked("posts");
+    try {
+      const result = await getAllUserComments(id);
+      setUserComments(result);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  // console.log(userLikes[0].post_id)
+
+
   return (
-    <div className="flex flex-row justify-center items-center w-full">
-      <div className="flex flex-col justify-around mb-[4rem]">
-        <img src={profileimage} alt="User Profile" className="rounded-full mb-[5rem] h-[15rem] w-[15rem]" />
-        {!!isCurrentUserProfile && <button onClick={handleLogout} className="w-[5rem] h-[2rem] bg-[#989A99] ml-[15rem] rounded-lg mt-[3rem] z-0">Log Out</button>}
-        <div className="flex flex-col items-center pt-[2rem] bottom-0 border-t-2 mr-[8rem] border-black z-1">
+    <div className="flex flex-row justify-center space-x-[3rem] pl-[10rem] pt-[5rem] items-center w-full h-full">
+      <div className="flex flex-col h-full w-[20rem] mt-[8rem]">
+        <img src={profileimage} alt="User Profile" className="rounded-full mb-[1rem] h-[15rem] w-[15rem]" />
+        {!!isCurrentUserProfile && <button onClick={handleLogout} className="w-[5rem] h-[2rem] bg-[#989A99] ml-[5rem] rounded-lg z-0">Log Out</button>}
+        <div className="flex flex-col items-center mt-[2rem] pt-[2rem] bottom-0 left-0 border-t-2 mr-[5rem] border-black z-1">
           <p>If the user had any data, it would be here</p>
           <p>Miscellaneous information or something</p>
         </div>
       </div>
 
-      <div className="h-full flex flex-col space-x-[8rem] mt-[2rem]">
-        <h1 className="text-3xl">{username}</h1>
-        <h2 className="text-xl mt-[20%]">{bio}</h2>
-        <div className="bg-[#989A99] flex flex-col mb-[10rem] z-0 right-0 bottom-0 items-center justify-center">
-          <p>If the user had any data, here it would be</p>
-          <p>Fake Bio or something ahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh</p>
+      <div className="h-full w-[40rem] flex flex-col left-0 pt-[5rem]">
+        <div className="flex flex-col h-full w-full">
+          <h1 className="text-3xl">{username}</h1>
+          <h2 className="text-xl mt-[2rem] pb-[6rem]">{bio}</h2>
+        </div>
+        <div className="flex flex-col w-full h-full">
+          <div className="flex flex-row align-start">
+            <button onClick={() => handlePostsClick(id)} className={`w-[5rem] h-[2rem] ${clicked === "posts" ? 'bg-[#989A99]' : 'bg-transparent'}`}>Posts</button>
+            <button onClick={() => handleLikesClick(id)} className={`w-[5rem] h-[2rem] ${clicked === "likes" ? 'bg-[#989A99]' : 'bg-transparent'}`}>Likes</button>
+          </div>
+          <div className="flex flex-col h-[15rem] w-[30rem] p-[2rem] mb-[10rem] z-0 bottom-0 border-t-2 border-black mb-[10rem] z-0 bottom-0 border-t-2 border-black">
+            {
+              clicked === "likes" && (
+                <ul className="flex flex-col">
+                  {
+                    userLikes.length > 0 ?
+                      userLikes.map((like, index) => <li key={index} className="text-3xl">{like.post_id}</li>)
+                      : <p>No likes yet</p>
+                  }
+                </ul>
+              )
+            }
+            {
+              clicked === "posts" && (
+                <ul className="flex flex-col">
+                  {
+                    userComments.length > 0 ?
+                      userComments.map((comment, index) => <li key={index} className="text-3xl">{comment.post_id}</li>)
+                      : <p>No comments yet</p>
+                  }
+                </ul>)
+            }
+          </div>
         </div>
       </div>
     </div>
