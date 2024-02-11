6b6ebdeeaacf73aea6e5756173a5518de38a2eba
Joeyyyys <josephgiler2@gmail.com>
2024-02-11 18:51:21 -0500

    modified the model and controller of post to include the new properties when creating a post

--- src/controllers/post/create-post.js
+++ src/controllers/post/create-post.js
@@ -2,10 +2,10 @@ const createPost = async (req, res) => {
     const {
         db: { Post },
         params: { user_id }, 
-        body: { title, description, location, image }, 
+        body: { title, description, location, image, start_time, end_time, tags}, 
     } = req;
 
-    const post = await Post.createPost(user_id, title, description, location, image);
+    const post = await Post.createPost(user_id, title, description, location, image, start_time, end_time, tags);
 
     Post ? res.status(201).send(post) : res.status(500).send('Internal Server Error');
   
