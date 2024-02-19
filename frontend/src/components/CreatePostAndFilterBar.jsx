840f35843855f9fa38ee6cebd8a049be1b292356
gonzzoh <gmeza0118@gmail.com>
2024-02-18 19:19:47 -0500

    Updated Css

--- frontend/src/components/CreatePostAndFilterBar.jsx
+++ frontend/src/components/CreatePostAndFilterBar.jsx
@@ -8,13 +8,15 @@ export default function CreatePostAndFilterBar({ posts, setPosts }) {
 
 
     return <>
-        <div className="w-[75%] flex flex-row items-center mt-2 pt-4 space-x-[2rem] absolute translate-y-[45%]">
-            <div className="basis-1/4 h-[5rem] bg-white p-4">
-                <h2>Sort By:</h2>
-            </div>
-            <div className="basis-2/4 h-[5rem] bg-white"><CreatePostForm posts={posts} setPosts={setPosts} /></div>
-            <div className="basis-1/4 h-[5rem] bg-white p-4">
-                <h2>Filter By:</h2>
+        <div className={`h-[15rem] w-full bg-community z-0 bg-cover bg-start flex align-middle content-center justify-center items-end overflow-visible`}>
+            <div className="w-[75%] flex flex-row items-center mt-2 pt-4 space-x-[2rem] absolute translate-y-[45%]">
+                <div className="basis-1/4 h-[5rem] bg-white p-4">
+                    <h2>Sort By:</h2>
+                </div>
+                <div className="basis-2/4 h-[5rem] bg-white"><CreatePostForm posts={posts} setPosts={setPosts} /></div>
+                <div className="basis-1/4 h-[5rem] bg-white p-4">
+                    <h2>Filter By:</h2>
+                </div>
             </div>
         </div>
     </>
--- frontend/src/pages/CommunityPosts.jsx
+++ frontend/src/pages/CommunityPosts.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
 import { getAllPosts } from "../adapters/post-adapter";
-import CreatePostAndFilterBar from "../components/CreatePostAndFilterBar";
 import { SimpleGrid, Card, CardHeader, Heading, CardBody, Text, CardFooter, Button } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
+import CreatePostAndFilterBar from "../components/CreatePostAndFilterBar";
+import CommunityPostsCard from "../components/CommunityPostsCard";
 export default function CommunityPosts() {
     const [posts, setPosts] = useState([]);
     const navigate = useNavigate();
@@ -10,7 +11,7 @@ export default function CommunityPosts() {
     useEffect(() => {
         const getPosts = async () => {
             const allPosts = await getAllPosts()
-            setPosts(allPosts.sort((a,b) => b.id - a.id))
+            setPosts(allPosts.sort((a, b) => b.id - a.id))
         }
         getPosts()
     }, [])
@@ -18,28 +19,27 @@ export default function CommunityPosts() {
 
     return <>
         <div className="w-full bg-[#D9D9D9]">
-            <div className={`h-[15rem] w-full bg-community z-0 bg-cover bg-start flex align-middle content-center justify-center items-end overflow-visible`}>
-                <CreatePostAndFilterBar posts={posts} setPosts={setPosts}/>
-            </div>
-            <div className='h-screen w-full bg-[#1C1E1F]'>
-                <SimpleGrid spacing={4} templateColumns='repeat(auto-fill, minmax(200px, 1fr))' className="pt-[4rem]">
-                    { posts.map((post) => {
-                    return <li key={post.id}>
-                    <Card>
-                        <CardHeader>
-                            <Heading size='md'>{post.title}</Heading>
-                        </CardHeader>
-                        <CardBody>
-                            <Text>{post.description}</Text>
-                        </CardBody>
-                        <CardFooter>
-                            <Button onClick={()=>{navigate(`/posts/${post.id}`)}}>View here</Button>
-                        </CardFooter>
-                    </Card>
-                    </li>})
-                    }
+            <CreatePostAndFilterBar posts={posts} setPosts={setPosts} />
+            <CommunityPostsCard />
+            {/* <div className='h-screen w-full bg-[#D9D9D9]flex flex-col justify-center p-[4rem]'>
+                <SimpleGrid spacing={4} templateColumns='repeat(auto-fill, minmax(200px, 1fr))' className="p-[4rem]">
+                    {posts.map((post) => {
+                        return <ul key={post.id}>
+                            <Card key={post.id}>
+                                <CardHeader>
+                                    <Heading size='md'>{post.title}</Heading>
+                                </CardHeader>
+                                <CardBody>
+                                    <Text>{post.description}</Text>
+                                </CardBody>
+                                <CardFooter>
+                                    <Button onClick={() => { navigate(`/posts/${post.id}`) }}>View here</Button>
+                                </CardFooter>
+                            </Card>
+                        </ul>
+                    })}
                 </SimpleGrid>
-            </div>
+            </div> */}
         </div>
     </>;
 }
