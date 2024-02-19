3e1a7922d36820d96072e5ddfa9bc3ade9ac9e1b
gonzzoh <gmeza0118@gmail.com>
2024-02-18 19:18:00 -0500

    Changed background

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -2,10 +2,8 @@ const knex = require('../knex');
 const { hashPassword, isValidPassword } = require('../../utils/auth-utils');
 
 class User {
-  #passwordHash = null; // a private property
+  #passwordHash = null;
 
-  // Why have a constructor here? We need a way to take the raw data returned from
-  // the database and hide the passwordHash before sending it back to the controller
   constructor({ id, username, password_hash, profile_image, bio, date_created }) {
     this.id = id;
     this.username = username;
@@ -18,7 +16,6 @@ class User {
   static async list() {
     const query = 'SELECT * FROM users';
     const { rows } = await knex.raw(query);
-    // use the constructor to hide each user's passwordHash
     return rows.map((user) => new User(user));
   }
 
@@ -53,17 +50,17 @@ class User {
     return knex.raw('TRUNCATE users;');
   }
 
-  static async delete(id){
+  static async delete(id) {
     const query = 'DELETE FROM users WHERE id = ? RETURNING *';
     const args = [id];
     const { rows: [deletedUser] } = await knex.raw(query, args);
-    return deletedUser ? new User(deletedUser) : null; // to see the instance of the deleted user
+    return deletedUser ? new User(deletedUser) : null;
   }
-  
-  update = async ( username, bio, profile_image ) => {
+
+  update = async (username, bio, profile_image) => {
     const rows = await knex('users')
       .where({ id: this.id })
-      .update({username, bio, profile_image})
+      .update({ username, bio, profile_image })
       .returning('*');
 
     const updatedUser = rows[0];
