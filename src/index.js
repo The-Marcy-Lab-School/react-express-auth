3d4ebe3c4a2ded91481727cd8c17469953e8715f
SamuelPickering <samuelpickering21@gmail.com>
2024-02-23 19:07:52 -0500

    Videos now display the names of the users on top their video

--- src/index.js
+++ src/index.js
@@ -12,14 +12,18 @@ const emailToSocketIdMap = new Map()
 const socketidToEmailMap = new Map()
 
 io.on('connection', socket => {
-  socket.on('join-room', (roomId, userId) => {
+  socket.on('join-room', (roomId, userId, username) => {
     socket.join(roomId)
     console.log(`${userId} in ${roomId}`)
-    socket.to(roomId).emit('user-connected',userId)
+    socket.to(roomId).emit('user-connected',userId, username)
     
-    socket.on('message', message => {
-      io.to(roomId).emit('createMessage', message)
+    socket.on('message', (message, username) => {
+      io.to(roomId).emit('createMessage', message, username)
     })
+    socket.on('video-state-change', (roomId, userId, videoEnabled) => {
+      // Broadcast video state change to all users in the room except the sender
+      io.to(roomId).emit('video-state-changed', userId, videoEnabled);
+  });
     socket.on('disconnect', () => {
       socket.to(roomId).emit('user-disconnected', userId)
     })
