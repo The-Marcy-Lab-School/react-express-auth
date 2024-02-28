abbe6b8fb6f15b8991c366df1382edbe7ad65a9f
AjBarton7 <Ajenebarton1@gmail.com>
2024-02-28 09:50:34 -0500

    added some new seeded info

--- src/db/seeds/event_relations.js
+++ src/db/seeds/event_relations.js
@@ -6,8 +6,16 @@ exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex('event_relations').del();
   await knex('event_relations').insert([
-    { user_id: 2, event_id: 1 },
-    { user_id: 4, event_id: 1 },
     { user_id: 5, event_id: 2 },
+    { user_id: 6, event_id: 2 },
+    { user_id: 7, event_id: 2 },
+    { user_id: 1, event_id: 3 },
+    { user_id: 3, event_id: 1 },
+    { user_id: 4, event_id: 2 },
+    { user_id: 2, event_id: 1 },
+    { user_id: 3, event_id: 2 },
+    { user_id: 4, event_id: 5 },
+    { user_id: 2, event_id: 5 },
+    { user_id: 6, event_id: 1 },
   ]);
 };
--- src/db/seeds/event_tags_events.js
+++ src/db/seeds/event_tags_events.js
@@ -16,6 +16,26 @@ const event_tags_events = [
     event_id: 2,
     event_tag_id: 7,
   },
+  {
+    event_id: 3,
+    event_tag_id: 1,
+  },
+  {
+    event_id: 4,
+    event_tag_id: 2,
+  },
+  {
+    event_id: 4,
+    event_tag_id: 5,
+  },
+  {
+    event_id: 5,
+    event_tag_id: 2,
+  },
+  {
+    event_id: 6,
+    event_tag_id: 1,
+  }
 ];
 exports.seed = async function (knex) {
   // Deletes ALL existing entries
