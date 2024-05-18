d3d9b7db33bb29dfffe91a103ff994a7f5e2a94d
benspector-mls <ben@marcylabschool.org>
2024-05-18 14:38:17 -0400

    adding controllers and routes

---
+++ server/controllers/followControllers.js
@@ -0,0 +1,25 @@
+const { isAuthorized } = require('../utils/auth-utils');
+const Follow = require('../db/models/Follow');
+
+// POST /api/users/:user_id/followers - create a new follower of user_id
+exports.createFollow = async (req, res) => {
+  const { user_id } = req.params
+  const follower_user_id = req.session.userId;
+
+  const newFollow = await Follow.create(follower_user_id, user_id);
+  res.send(newFollow);
+};
+
+// GET /api/users/:user_id/followers - get followers of user_id
+exports.getFollowers = async (req, res) => {
+  const { user_id } = req.params
+  const followers = await Follow.findFollowersOf(user_id)
+  res.send(followers || []);
+}
+
+// GET /api/users/:user_id/follows - Get follows by user_id
+exports.getFollows = async (req, res) => {
+  const { user_id } = req.params
+  const following = await Follow.findFollowsBy(user_id)
+  res.send(following || []);
+}
--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -1,24 +1,28 @@
 const express = require('express');
 const userControllers = require('../controllers/userControllers');
 const postControllers = require('../controllers/postControllers');
+const followControllers = require('../controllers/followControllers');
 const checkAuthentication = require('../middleware/checkAuthentication');
 
 const userRouter = express.Router();
 
-userRouter.post('/:user_id/posts', postControllers.createPost);
+// Follows
+userRouter.post('/:user_id/followers', checkAuthentication, followControllers.createFollow)
+userRouter.get('/:user_id/followers', followControllers.getFollowers)
+userRouter.get('/:user_id/follows', followControllers.getFollows)
+
+// Posts
+userRouter.post('/:user_id/posts', checkAuthentication, postControllers.createPost);
+userRouter.delete('/:user_id/posts/:post_id', checkAuthentication, postControllers.deletePost);
+userRouter.delete('/:user_id/posts', checkAuthentication, postControllers.deleteAllPostsByUser);
 userRouter.get('/:user_id/posts', postControllers.getPostsByUserId);
-userRouter.delete('/:user_id/posts/:post_id', postControllers.deletePost);
-userRouter.delete('/:user_id/posts', postControllers.deleteAllPostsByUser);
 userRouter.get('/posts', postControllers.getAllPosts);
-
-
-userRouter.post('/', userControllers.createUser);
 
 // These actions require users to be logged in (authentication)
 // Express lets us pass a piece of middleware to run for a specific endpoint
+userRouter.post('/', userControllers.createUser);
 userRouter.get('/', checkAuthentication, userControllers.listUsers);
 userRouter.get('/:id', checkAuthentication, userControllers.showUser);
 userRouter.patch('/:id', checkAuthentication, userControllers.updateUser);
-
 
 module.exports = userRouter;
