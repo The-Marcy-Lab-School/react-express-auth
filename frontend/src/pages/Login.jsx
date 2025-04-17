1abe407bf537e860534179afe73c2c79e048ea9a
benspector-mls <ben@marcylabschool.org>
2025-04-17 09:47:04 -0400

    adding controlled form logic

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -6,6 +6,8 @@ import CurrentUserContext from "../contexts/current-user-context";
 export default function LoginPage() {
   const navigate = useNavigate();
   const [errorText, setErrorText] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   const { currentUser, setCurrentUser } = useContext(CurrentUserContext);
 
   // users shouldn't be able to see the login page if they are already logged in.
@@ -16,9 +18,10 @@ export default function LoginPage() {
   const handleSubmit = async (event) => {
     event.preventDefault();
     setErrorText('');
-    const formData = new FormData(event.target);
-    const [user, error] = await logUserIn(Object.fromEntries(formData));
+
+    const [user, error] = await logUserIn({ username, password });
     if (error) return setErrorText(error.message);
+
     setCurrentUser(user);
     navigate(`/users/${user.id}`);
   };
@@ -28,10 +31,10 @@ export default function LoginPage() {
     <form onSubmit={handleSubmit} aria-labelledby="login-heading">
       <h2 id='login-heading'>Log back in!</h2>
       <label htmlFor="username">Username</label>
-      <input type="text" autoComplete="username" id="username" name="username" />
+      <input type="text" autoComplete="username" id="username" name="username" value={username} onChange={(e) => setUsername(e.target.value)} />
 
       <label htmlFor="password">Password</label>
-      <input type="password" autoComplete="current-password" id="password" name="password" />
+      <input type="password" autoComplete="current-password" id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
 
       <button>Log in!</button>
     </form>
