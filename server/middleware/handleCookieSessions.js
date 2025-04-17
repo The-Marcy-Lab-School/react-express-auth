77bd3c2bf73180ef83165f80c0ea34709e1177b3
benspector-mls <ben@marcylabschool.org>
2025-04-17 12:25:48 -0400

    removing confusing comment

--- server/middleware/handleCookieSessions.js
+++ server/middleware/handleCookieSessions.js
@@ -2,12 +2,6 @@ const cookieSession = require('cookie-session');
 const handleCookieSessions = cookieSession({
   name: 'session',
   secret: process.env.SESSION_SECRET,
-  // By default, the cookie's lifetime is "session"
-  // which means until we close the browser. We like this for now!
-  // But in real life you'd set the cookie to expire,
-  // and implement an auto re-auth flow, but that's too much at this point.
-
-  // maxAge: 1000 * 60 * 60 * 24  // 24 hours
 });
 
 module.exports = handleCookieSessions;
